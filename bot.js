557140e9e188e7b56d7996b04035a025d316d7f4
Capuccino <aguilarralph13@gmail.com>
2016-09-26 13:35:31 +0800

    added files
    /commands/ folder coming soon
    
    go awau

---
+++ bot.js
@@ -0,0 +1,78 @@
+import {Discord} from "discord.js";
+import {fs} from "fs";
+import {util} from "util";
+
+const config = require('./botConfig.json');
+const bot = new Discord.Client({
+    autoRecconect: true
+});
+
+bot.on("ready", function () {
+    require('./init_commands.js').init();
+    console.log("Auth token :" + config.auth);
+
+});
+//don't place anything here. commands has their own JS files
+const commands = {};
+class Commands { constructor() { } };
+exports.addCommand = function (commandName, commandObject) {
+    try {
+        commands[commandName] = commandObject;
+    } catch (err) {
+        console.log(err);
+    }
+}
+exports.commandCount = function () {
+    return Object.keys(commands).length;
+};
+
+bot.on("message", function () {
+    //giant ass CMD thing CP'ed from flan-chanbot.
+    if (msg.content.startsWith(config.prefix)) {
+        const noPrefix = msg.content.substring(config.prefix.length, msg.content.length);
+        const noPrefixSplit = noPrefix.split(" ");
+        const cmd = noPrefixSplit[0];
+        const preArgs = msg.content.substring(prefix.length + noPrefixSplit[0], msg.content.length);
+        const args = preArgs.split(" ");
+        const suffix = msg.content.substring(prefix.length + noPrefixSplit[0], msg.content.length);
+        suffix.split();
+        args.shift();
+        if (commands[cmd] !== undefined) {
+            if (commands[cmd].adminOnly) {
+                fs.readFile('./adminList.json', (err, listData) => {
+                    if (!err) {
+                        var adminList = JSON.parse(listData);
+                        if (adminList.indexOf(msg.author.id) > -1 || msg.author.id === config.ownerID) {
+                            commands[cmd].main(bot, msg, args);
+                        } else {
+                            bot.sendMessage(msg, util.format('I\'m sorry, but you need to be on the admin list in order to run this command.'));
+                        }
+                    } else if (err) {
+                        bot.sendMessage(msg, `Experienced error while trying to execute command \`${cmd}\`.
+\`\`\`
+${err}
+\`\`\``);
+                    }
+                });
+            } else {
+                fs.readFile('./blackList.json', (err, blData) => {
+                    if (!err) {
+                        var blackList = JSON.parse(blData);
+                        if (blackList.indexOf(msg.author.id) === -1) {
+                            commands[cmd].main(bot, msg, args);
+                        }
+                    } else if (err) {
+                        clementine.sendMessage(msg, `Experienced error while trying to execute command \`${cmd}\`.
+\`\`\`
+${err}
+\`\`\``);
+                    }
+                });
+            }
+        }
+    }
+
+    if (msg.content.startsWith("<@" + bot.user.id + "> prefix")) {
+        clementine.reply(msg.channel.id, "***My prefix is *** `" + prefix + "`!");
+    }
+})
