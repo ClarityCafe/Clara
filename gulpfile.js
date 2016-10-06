e82a3dd40e51f86c7cd63ac84dfa7334ca6a940b
Capuccino <aguilarralph13@gmail.com>
2016-10-06 11:26:53 +0800

    added gulp build checker

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ const babel = require('gulp-babel');
 gulp.task('build', () => {
     return gulp.src('src')
         .pipe(babel({
-            presets: ['es2015']
-            minified: true
-            comments: false
+            presets: ['es2015'],
+            minified: true,
+            comments: false,
             babelrc: true
         }))
         .pipe(gulp.dest('out'));
