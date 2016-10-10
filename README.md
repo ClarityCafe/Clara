32f00c69b55622ca4799505b4973af8113a759c7
Ovyerus <iamovyerus@gmail.com>
2016-10-10 17:58:12 +1100

    Updated example.js and README.md (#4)
    * Improvements
    
    * Updated example command
    
    * Update example command comments and cleaned up some spelling in README.md

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # owo-whats-this [![Build Status](https://travis-ci.org/sr229/owo-whats-this.svg?branch=master)](https://travis-ci.org/sr229/owo-whats-this)
-a really experimental (and borked) Discord.js bot wirtten in EsNEXT (Es6)
+A really experimental (and borked) Discord.js bot written in EsNEXT (Es6)
 
 # Running
 
-you can't run it (yet). You have to compile using babel in order to make it work through Node.js
+From a fresh download/clone, you cannot run the script directly. You have to compile it using [Babel](https://babeljs.io/) in order to make it work through Node.js
 
-do ``npm -g install babel babel-cli`` and the ES2015 preset for babel.
+``npm -g install babel babel-cli`` and use the 'ES2015' preset for Babel.
 
 the rest is pretty straightforward if you've used Babel before.
 
-#testing
+# Testing
 
-Contributions/commits/PRs are tested by Travis CI. If your PR fails, it won't be merged until the following is fixed.
-this is to make sure the cocurrent code would work on optimum state
+Contributions/commits/PRs are tested by Travis CI. If your PR fails, it won't be merged until it is fixed.
+This is to make sure the cocurrent code would work on optimum state.
--- example/sample_command/example.js
+++ example/sample_command/example.js
@@ -1,19 +1,26 @@
 exports.commands = [
-    //add a keyword for your command here
-    "example"
+    // Define the name of your commands here.
+    'ping',
+    'pong'
 ];
 
-//the proper format for your command is
-exports.example = {
-    desc: "", //the description for this command-specific help
-    longDesc: "", //used by the <prefix>help command.
-    main: 
-    //process goes here. a Helloworld example has been given
-    // bot = the bot (always append this)
-    // msg = the keyword initializer
-    // suffix = for a non-argument userentry
-    //args = command-specific args
-    function(bot, ctx) {
-        ctx.msg.channel,sendMessage('Hello world');
+// Example ping command
+exports.ping = {
+    desc: '', // Mandatory - Description displayed in the main help command.
+    longDesc: '', // Optional - Description used in 'help [command name]'. If not supplied, default description will be used
+    usage: '', // Optional - Displayed in the help command. Describes any arguments the command accepts if any. Format: <required argument> [optional argument]
+    main: function(bot, ctx) { // Mandatory - The actual command process.
+    // 'bot' is the Discord.JS Client object. Is extended with 'config' which is the bot configuration file, and 'data' which contains the blacklist and adminlist.
+    // 'ctx' is a context object. Contains: 'msg' - A Discord.JS Message object; 'args' - An array where each value is a string, EG: if the command message was 'help ping', args[0] would be 'ping'; 'suffix' - The same as args, but instead of an array, is a string of all the args together, seperated with spaces.
+        ctx.msg.channel,sendMessage('Ping!');
+    }
+}
+
+// Example pong command (demonstrating a full command, and multiple commands in a file).
+exports.pong = {
+    desc: 'Ping!',
+    longDesc: 'Simple response command.',
+    main: function(bot, ctx) {
+        ctx.msg.channel.sendMessage('Pong!');
     }
 }
