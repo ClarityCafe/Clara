964fc8a61ffd17372c362e1d2d700db17e1321d1
sr229 <aguilarralph13@gmail.com>
2016-10-13 17:54:46 +0800

    added WIP music command

---
+++ src/commands/music/todokete.js
@@ -0,0 +1,30 @@
+exports.commands = [
+    "play",
+    "add",
+    "remove",
+    "clear",
+    "queue"
+];
+const request = require('request');
+const fs = require('fs');
+const youtubedl = require('youtube-dl');
+const m3u8 = require('mu38');
+const stream = require('stream');
+
+var startup
+var inviteURL;
+var permissions = '0';
+var music = {
+    summoners: {};
+    votes: {};
+    songQ: {};
+    streams: {};
+}
+fs.mkdir('songs', 0777, function(err) {
+if (err) {
+if (err.code == "EEXIST") return;
+else throw err;
+}
+});
+});
+})
