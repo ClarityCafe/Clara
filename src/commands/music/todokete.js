0d3cea2affdbb7d6a67a9b523d32c8d9ee4f005c
sr229 <aguilarralph13@gmail.com>
2016-10-13 22:48:13 +0800

    refactored something

--- src/commands/music/todokete.js
+++ src/commands/music/todokete.js
@@ -3,7 +3,9 @@ exports.commands = [
     "add",
     "remove",
     "clear",
-    "queue"
+    "queue",
+    "summon",
+    "disconnect"
 ];
 const request = require('request');
 const fs = require('fs');
@@ -11,18 +13,21 @@ const youtubedl = require('youtube-dl');
 const m3u8 = require('mu38');
 const stream = require('stream');
 
-var startup
-var inviteURL;
-var permissions = '0';
-var music = {
-    summoners: {};
-    votes: {};
-    songQ: {};
-    streams: {};
-}
-fs.mkdir('songs', 0777, function(err) {
-    if (err) {
-        if (err.code == "EEXIST") return;
-        else throw err;
+var voice_channel = {};
+var ffmpegs = {};
+var songQ = {};
+var summoners = {};
+var votes = {};
+
+exports.summon = {
+    desc: 'summon the bot to a voice channel',
+    longDesc: 'summon the bot to specific voice channel. you need to be in the target channel to summon the bot there.',
+    main: function(bot, ctx) {
+        voice_channel(voice_channel_id, function() {
+            summoners[serverID] = userID;
+            voice_channel[serverID] = voice_channel_id;
+            ctx.msg.channel.sendMessage("Ready to Play, Imouto!");
+        });
     }
-});
+
+}
