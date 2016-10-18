ad0a70f9ab3ddc6d6a35b3c39d58c96d9b1b07a2
sr229 <aguilarralph13@gmail.com>
2016-10-18 10:26:38 +0800

    ported help command

---
+++ src/commands/help/abalabahaha.js
@@ -0,0 +1,37 @@
+exports.commands = [
+    "help"
+];
+exports.help = {
+    name: "help",
+    desc: "This help command.",
+    longDesc: "The help command. Give it an argument to see more information about a command.",
+    usage: "[command]",
+    main: function(bot, ctx) {
+        if (args.length === 0) {
+            var helpStart = util.format("%s - %s\n", config.botName, config.botDesc);
+            var helpThing = "";
+            var cmd;
+            for (cmd in commands) {
+                if (commands[cmd].usage !== undefined) {
+                    var command = util.format("**%s %s** - %s\n", commands[cmd].name, commands[cmd].usage, commands[cmd].desc);
+                    helpThing += command;
+                } else {
+                    var command = util.format("**%s** - %s\n", commands[cmd].name, commands[cmd].desc);
+                    helpThing += command;
+                }
+            }
+            ctx.sendMessage(msg, "Help has been sent to your DMs.")
+            ctx.sendMessage(msg.author.id, helpStart + helpThing);
+        } else if (args.length === 1) {
+            var cmd = commands[args[0]];
+            if (cmd !== undefined && cmd.usage !== undefined) {
+                ctx.sendMessage(msg, util.format("**%s %s** - %s", cmd.name, cmd.usage, cmd.longDesc));
+            } else if (cmd !== undefined) {
+                ctx.sendMessage(msg, util.format("**%s** - %s", cmd.name, cmd.longDesc));
+            } else if (cmd === undefined) {
+                ctx.reply(msg, "I'm sorry, but that command does not seem to exist.");
+            }
+        }
+    }
+}
+}
