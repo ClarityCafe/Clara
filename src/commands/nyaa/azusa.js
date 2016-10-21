dc92d543685c4da9d2a600240c8b368cf50394c9
sr229 <aguilarralph13@gmail.com>
2016-10-21 22:52:01 +0800

    updated things

--- src/commands/nyaa/azusa.js
+++ src/commands/nyaa/azusa.js
@@ -6,7 +6,12 @@ exports.nyaa = {
     desc: "Nyaaa!",
     longDesc: "Ny- Nyaaaaaaaaaaaaaaaaaaaaaaaaaa~",
     main: function(bot, ctx) {
-        var url = azusa.get();
+        var url = azusa.get({
+            // just to make sure Gus never whines
+            //about ant-sized images.
+            width: 1080,
+            height: 1920
+        });
         ctx.msg.channel.sendMessage(url);
     }
 }
