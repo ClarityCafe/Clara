08f9ce0d0b267b1ed46183ad83f09854c8267b0a
sr229 <aguilarralph13@gmail.com>
2016-11-07 16:17:54 +0800

    added ban command
    if you thought kick is too deep, watch this.

--- src/commands/moderation/moderation.js
+++ src/commands/moderation/moderation.js
@@ -28,3 +28,23 @@ exports.kick = {
                 }
             }
         }
+        exports.ban = {
+            name: "ban",
+            desc: `Bans a user from the server. Requires the bot to have the "Administrator" or the "Manage Users" Permission`,
+            usage: "<User Mention>",
+            main: (bot, ctx) => {
+                if (ctx.msg.guild) {
+                    if (ctx.msg.mentions.users.length === 0) {
+                        if (ctx.msg.member.hasPermission('BAN_MEMBERS')) {
+                            ctx.msg.guild.member(ctx.msg.mentions.users.first()).ban().then(u => {
+                                ctx.msg.channel.sendMessage(`Banned ${u.username}#${u.discriminator}`);
+                            }).catch(err => {
+                                //handle error
+                            });
+                        } else {
+                            ctx.msg.channel.sendMessage('You requie the **Ban Members** permission to execute this command.');
+                        }
+                    }
+                }
+            }
+        }
