3e8c4025cb8f1ef622743e65c4265256c386627c
sr229 <aguilarralph13@gmail.com>
2016-11-08 17:52:15 +0800

    changed syntactic sugar on example to ES6
    Ovy

--- example/sample_command/example.js
+++ example/sample_command/example.js
@@ -11,7 +11,7 @@ exports.ping = {
     longDesc: '', // Optional - Description used in 'help [command name]'. If not supplied, default description will be used
     adminOnly: true, // Optional - Boolean. Make the command only work for those who are in the adminlist. Recommended if your command is volatile and recommended only for those who are mentally sane to use it. Implementation Coming soon.
     usage: '', // Optional - Displayed in the help command. Describes any arguments the command accepts if any. Format: <required argument> [optional argument]
-    main: function(bot, ctx) { // Mandatory - The actual command process.
+    main: (bot,ctx) => { // Mandatory - The actual command process.
         // 'bot' is the Discord.JS Client object. Is extended with 'config' which is the bot configuration file, and 'data' which contains the blacklist and adminlist.
         // 'ctx' is a context object. Contains: 'msg' - A Discord.JS Message object; 'args' - An array where each value is a string, EG: if the command message was 'help ping', args[0] would be 'ping'; 'suffix' - The same as args, but instead of an array, is a string of all the args together, seperated with spaces.
         ctx.msg.channel, sendMessage('Ping!');
@@ -22,7 +22,7 @@ exports.ping = {
 exports.pong = {
     desc: 'Ping!',
     longDesc: 'Simple response command.',
-    main: function(bot, ctx) {
+    main: (bot, ctx) => {
         ctx.msg.channel.sendMessage('Pong!');
     }
 }
