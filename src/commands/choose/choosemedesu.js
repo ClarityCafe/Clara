0fb5f6976c95a5b3eed7fcc4120daa0d67aea5a8
Capuccino <aguilarralph13@gmail.com>
2016-11-17 12:40:09 +0800

    use suffix instead of args
    who made the idea of using spaces instead of visible characters to split decisions?

--- src/commands/choose/choosemedesu.js
+++ src/commands/choose/choosemedesu.js
@@ -16,10 +16,10 @@ exports.choose = {
     usage: '<choices (minimum of two)>',
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
-            if (ctx.args.length < 2) {
+            if (ctx.suffix.split("/")) {
                 ctx.msg.channel.sendMessage('Please give me at least `two` (2) arguments.').then(() => reject([new Error('Not enough arguments given.')])).catch(err => reject([err]));
             } else {
-                var choice = ctx.args[Math.floor(Math.random() * ctx.args.length)];
+                var choice = ctx.suffix[Math.floor(Math.random() * ctx.suffix.length)];
                 ctx.msg.channel.sendMessage(`**${ctx.msg.author.username}#${ctx.msg.author.discriminator}**, I choose \`${choice}\`!`).then(() => {
                     resolve();
                 }).catch(err => reject([err]));
