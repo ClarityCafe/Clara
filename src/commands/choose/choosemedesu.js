17358a8f8cd6494fd4f842fc8c667a316866225d
Ovyerus <iamovyerus@gmail.com>
2016-11-17 17:27:45 +1100

    fix choose command

--- src/commands/choose/choosemedesu.js
+++ src/commands/choose/choosemedesu.js
@@ -16,10 +16,11 @@ exports.choose = {
     usage: '<choices (minimum of two)>',
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
-            if (ctx.suffix.split("/")) {
+            var choices = ctx.suffix.split('/');
+            if (choices.length < 2) {
                 ctx.msg.channel.sendMessage('Please give me at least `two` (2) arguments.').then(() => reject([new Error('Not enough arguments given.')])).catch(err => reject([err]));
             } else {
-                var choice = ctx.suffix[Math.floor(Math.random() * ctx.suffix.length)];
+                var choice = choices[Math.floor(Math.random() * choices.length)];
                 ctx.msg.channel.sendMessage(`**${ctx.msg.author.username}#${ctx.msg.author.discriminator}**, I choose \`${choice}\`!`).then(() => {
                     resolve();
                 }).catch(err => reject([err]));
