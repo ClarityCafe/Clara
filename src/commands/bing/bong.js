eba4b46f3b2b7a780108d0a54ccf915b26044d37
sr229 <aguilarralph13@gmail.com>
2016-11-18 18:38:40 +0800

    added bing command w

---
+++ src/commands/bing/bong.js
@@ -0,0 +1,29 @@
+/* Bing - hi
+ *  Contributed by :
+ * | Capuccino
+ * 
+ */
+exports.commands = [
+    "bing",
+    "bing-img"
+];
+
+//can't we use ES6 memes here?
+const bing = require("bing");
+const Promise = require("bluebird");
+const logger = require(`${_baseDir}/lib/logger.js`);
+
+exports.bing = {
+    desc:"queries a search team from Bing.",
+    fullDesc:"searches using a term provided using Bing.",
+    usage: "<Search term>",
+    main: (bot,ctx) => {
+        return new Promise((resolve,reject) => {
+            function searchTerm(search) {
+                //placeholder for now
+                //refer to http://www.bing.com/developers/s/apibasics.html
+                //and https://github.com/jwalgran/bing
+            }
+        })
+    }
+}
