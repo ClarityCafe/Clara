51de86a5fb1a8039513d225b326608acf9d3d92b
sr229 <aguilarralph13@gmail.com>
2016-11-18 19:22:08 +0800

    ovy pls add moar kthx bai

--- src/commands/bing/bong.js
+++ src/commands/bing/bong.js
@@ -9,21 +9,26 @@ exports.commands = [
 ];
 
 //can't we use ES6 memes here?
-const bing = require("node-bing-api")({});
+const bing = require("node-bing-api")({ acckey: `${config.bingAPIkey}` });
+const config = require(`${__baseDir}/config.json`);
 const Promise = require("bluebird");
-const logger = require(`${_baseDir}/lib/logger.js`);
 
 exports.bing = {
-    desc:"queries a search team from Bing.",
-    fullDesc:"searches using a term provided using Bing.",
+    desc: "queries a search team from Bing.",
+    fullDesc: "searches using a term provided using Bing.",
     usage: "<Search term>",
-    main: (bot,ctx) => {
-        return new Promise((resolve,reject) => {
-            function searchTerm(search) {
-                //placeholder for now
-                //refer to http://www.bing.com/developers/s/apibasics.html
-                //and https://github.com/goferito/node-bing-api
-            }
-        })
+    main: (bot, ctx) => {
+        if (ctx.suffix) {
+            return new Promise((resolve, reject) => {
+                //this would return only the top search result
+                //any other entries are disregarded
+                bing.web(`${ctx.suffix}`,{top: 1, skip: 0}, (err,res,body) => {
+                    var searchResult = body.webPages.value[0,1];
+                    ctx.msg.channel.sendMessage(searchResult).then(() => resolve()).catch(err => reject([err]));
+                })
+            })
+        }
     }
-}
+},
+
+//refer to https://github.com/goferito/node-bing-api for API usage.
