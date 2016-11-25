c3ac379e377b3bf6a884b54c382a8c3a6b6600f6
Ovyerus <iamovyerus@gmail.com>
2016-11-25 21:23:27 +1100

    :NotLikeMiya:

--- src/commands/child_process_hosts/remote_process_ctrl.js
+++ src/commands/child_process_hosts/remote_process_ctrl.js
@@ -1,22 +1,23 @@
-/* Basic Remote PM2 controller
- *  Contibuted by :
- * | Capuccino
+/*
+ * remote_process_ctrl.js - control pm2 from Discord
  * 
+ * Contributed by Capuccino
  * NOTE: This is dedicated for PM2 and will ONLY work for PM2.
- *  
  */
-exports.command = [
-    "core_restart",
-    "core_shutdown"
-];
 
 const Promise = require('bluebird');
 const ovy = require('child_process');
-const bot = ovy.exec('pm2', [`${process_react}`,'bot']);
+const bot = ovy.exec('pm2', [process_react,'bot']);
+
+exports.command = [
+    'core_restart',
+    'core_shutdown'
+];
+
 exports.core_restart = {
-    desc : "BOTDEVELOPER ONLY! Restarts the PM2 session of the bot",
-    adminOnly : "true",
-    main : (bot , ctx) => {
+    desc : 'BOTDEVELOPER ONLY! Restarts the PM2 session of the bot',
+    adminOnly : true,
+    main : (bot, ctx) => {
         return new Promise((resolve,reject) => {
             var process_react = "restart";
             //TODO: add a clock to delay the command for 3 seconds
