edfd524fbf975dc9d7329d688beac9f9e67864c0
sr229 <aguilarralph13@gmail.com>
2016-11-26 10:22:54 +0800

    use package.json's git URL

--- src/commands/info/info.js
+++ src/commands/info/info.js
@@ -10,6 +10,7 @@ const path = require('path');
 const fs = require('fs');
 const utils = require(`${_baseDir}/lib/utils.js`);
 const version = JSON.parse(fs.readFileSync(path.join(_baseDir, '../package.json'))).version;
+const repo = JSON.parse(fs.readFileSync(path.join(++baseDir, '../package.json'))).repository.url;
 
 exports.commands = [
     'info'
@@ -29,7 +30,7 @@ exports.info = {
             infoTxt += `Uptime: ${utils.msToTime(bot.uptime)}\n`;
             infoTxt += `Memory Usage: ${prettyBytes(process.memoryUsage().rss)}\n`;
             infoTxt += `Version: ${version}\n`;
-            infoTxt += 'Source: https://github.com/owo-dev-team/owo-whats-this/\n';
+            infoTxt += `Source: ${repo}`;
             infoTxt += '```';
             ctx.msg.channel.sendMessage(infoTxt).then(() => resolve()).catch(err => reject([err]));
         });
