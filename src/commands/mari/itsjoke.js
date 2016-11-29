9db7461bd01f1d71317221da02597f3432990f70
sr229 <aguilarralph13@gmail.com>
2016-11-29 17:03:01 +0800

    I don't know anymore

---
+++ src/commands/mari/itsjoke.js
@@ -0,0 +1,24 @@
+/* It's Joke!
+ *
+ * Contributed by Ohara Mari.
+ * 
+ * Don't ask why is this a thing.
+ * 
+ */
+
+exports.command = [
+    "itsjoke"
+];
+
+const Promise = require('bluebird');
+
+exports.itsjoke = {
+    desc: "it's Joke!",
+    longDesc: "it's Joke!",
+    usage: "<It's Joke>",
+    main: (bot, ctx) => {
+        return new Promise((reject, resolve) => {
+            ctx.msg.channel.sendFile('../res/itsjoke.png', 'itsjoke.png');
+        })
+    }
+}
