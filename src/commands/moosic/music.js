89fe0618355603246a69578bbc89ec264932df2c
sr229 <aguilarralph13@gmail.com>
2016-12-01 15:04:18 +0800

    quak

---
+++ src/commands/moosic/music.js
@@ -0,0 +1,18 @@
+/* Music Command
+ * 
+ * Contributed by Capuccino
+ *
+ */
+exports.commands = [
+    "play",
+    "skip",
+    "disconnect",
+];
+
+exports.play = {
+    desc: "nani sore wat the fuck are u doing ovy aaaaaaa",
+    usage: "<YT Link/Song Link>",
+    main: (bot,ctx) => {
+        
+    }
+}
