313c09f4ec06693f8feb72642ddae22423fb9b5c
Capuccino <chinodesuuu@gmail.com>
2016-12-02 04:12:21 +0000

    fuck you eslint

--- src/commands/moosic/music.js
+++ src/commands/moosic/music.js
@@ -12,14 +12,18 @@ exports.commands = [
 const Promise = require('bluebird');
 const ytdl = require('youtube-dl');
 var queue = [];
-var nowPlaying = "";
+var nowPlaying = '';
 
 
 
 exports.play = {
-    desc: "nani sore wat the fuck are u doing ovy aaaaaaa",
-    usage: "<YT Link/Song Link>",
+    desc: 'nani sore wat the fuck are u doing ovy aaaaaaa',
+    usage: '<YT Link/Song Link>',
     main: (bot, ctx) => {
-        return new Promise((reject, resolve) => {});
+        return new Promise((reject, resolve) => {
+            if (ctx.suffix) {
+                ytdl('')
+            }
+        });
     }
 };
