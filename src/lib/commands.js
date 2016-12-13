3c012ead2e5c8ecc4dc45b0b6d89b13f6de2e9b5
Capuccino <aguilarralph13@gmail.com>
2016-12-13 12:43:58 +0800

    removed classed-based shit after some feedback

--- src/lib/commands.js
+++ src/lib/commands.js
@@ -5,7 +5,7 @@ const commands = {};
 //TODO: always overwrite command object when there is a existing one present
 //old functions to add/remove commands. DO NOT REMOVE until the classes are tested to work properly!
 
-/*function addCommand(cmdName, cmdObject) {
+function addCommand(cmdName, cmdObject) {
     return new Promise((resolve, reject) => {
         if (typeof cmdName !== 'string') reject(new Error('cmdName is not a string'));
         if ((cmdObject instanceof Object) !== true) reject(new Error('cmdObject is not an object'));
@@ -19,9 +19,9 @@ const commands = {};
             reject(new Error(`'${cmdName}' already exists in the command object.`));
         }
     });
-}*/
+}
 
-/*function removeCommand(cmdName) {
+function removeCommand(cmdName) {
     return new Promise((resolve, reject) => {
         if (typeof cmdName !== 'string') reject(new Error('cmdName is not a string.'));
         if (!commands[cmdName]) reject(new Error(`'${cmdName}' does not exist in the command object.`));
@@ -31,35 +31,7 @@ const commands = {};
         resolve(cmdName);
     });
 }
-*/
 
-//new methods for add/remove commands.
-
-class addCommand extends commands {
-    constructor() {
-        return new Promise((resolve, reject) => {
-            if (typeof cmdName !== 'string') reject('cmdName is not a string');
-            if ((cmdObject instanceof Object) !== true) reject(new Error('cmdObject is not an object'));
-            if (commands[cmdName] == 'undefined') {
-                commands[cmdName] = cmdObject;
-                resolve(commands[cmdName][cmdObject]);
-            } else {
-                reject(`'${cmdName} already exists in the current Object.`);
-            }
-        });
-    }
-}
-
-class removeCommand extends commands {
-    constructor() {
-        return new Promise((resolve, reject) => {
-            if (!commands[cmdName]) reject(new Error(`'${cmdName}' does not exist in the command object.`));
-            if (commands[cmdName].fixed) reject(new Error(`'${cmdName}' cannot be removed.`));
-            delete commands[cmdName];
-            resolve(cmdName);
-        });
-    }
-}
 
 exports.commands = commands;
 exports.addCommand = addCommand;
