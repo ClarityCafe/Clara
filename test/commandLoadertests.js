14a29e4f0754d0ba0655e703be90b9718016ca37
Capuccino <aguilarralph13@gmail.com>
2016-12-15 12:28:23 +0800

    whooo dummeh bottu

--- test/commandLoadertests.js
+++ test/commandLoadertests.js
@@ -1,3 +1,11 @@
+/* Jasmine Spec file
+ * 
+ * command parse validator. used for Travis.
+ * 
+ * Contributed by Capuccino
+ * 
+ */
+
 const jasmine = require('jasmine');
 const Discord = require('discord.js');
 const Promise = require('bluebird');
@@ -6,11 +14,28 @@ const logger = require(`${__dirname}/logger.js`);
 const commandLoader = require(`${__dirname}/src/lib/commandLoader`);
 const commands = require(`${__dirname}/src/lib/commands.js`);
 
+var awau  = [];
+
+var owo   = [];
 
 jasmine.describe('loadCommandsTest', () => {
     jasmine.it('simulate_bot', () => {
         return new Promise((resolve,reject) => {
-            
+            bot.on('ready', () => {
+                logger.info('dummy bot initialized');
+                require(commandLoader).init().then(() => {
+                    logger.info(`Dry-run command parsing has finished, loaded ${Object.keys(bot.commands).length} ${Object.keys(bot.commands).length === 1 ? 'command' : 'commands' }`);
+                    return awau;
+                }).catch(err => {
+                    console.error(`experienced an error with a command!`);
+                    return owo;
+                });
+            });
+            exports.addCommand = commands.addCommand;
+            exports.removeCommand = commands.removeCommand;
+            exports.bot = bot;
         });
     });
 });
+jasmine.expect();
+jasmine.expect();
