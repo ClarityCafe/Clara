41d9f46c9ad84af0e15781c804230158f00ed92c
Capuccino <aguilarralph13@gmail.com>
2017-01-09 14:27:46 +0800

    fixed conflict

--- src/commands/mari/itsjoke.js
+++ src/commands/mari/itsjoke.js
@@ -13,23 +13,17 @@ exports.command = [
     'itsjoke'
 ];
 
-<<<<<<< HEAD
+
 const fs = require('fs');
 const Promise = require('bluebird');
 
-=======
->>>>>>> b3f7bf06631ca2776351ada74f01b9eff844e5ee
 exports.itsjoke = {
     desc: "it's Joke!",
     longDesc: "it's Joke!",
     usage: "<It's Joke!>",
     main: (bot, ctx) => {
         return new Promise((reject, resolve) => {
-<<<<<<< HEAD
             ctx.msg.channel.createMessage({file: fs.readFileSync('../res/528.jpg'), name: 'joke.jpg'}).then(() => resolve()).catch(err => reject([err]));
-=======
-              ctx.msg.channel.createMessage({file: fs.readFileSync('../res/528.jpg'), name: 'joke.jpg'}).then(() => resolve()).catch(err => reject([err]));
->>>>>>> b3f7bf06631ca2776351ada74f01b9eff844e5ee
         });
     }
 };
