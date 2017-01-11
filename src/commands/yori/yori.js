63ec8a4414a21bdafa173e3e888806c061deaccb
Ovyerus <iamovyerus@gmail.com>
2017-01-11 16:56:46 +1100

    Add yori command for random anime pictures

--- src/commands/yori/yori.js
+++ src/commands/yori/yori.js
@@ -1,30 +1,60 @@
-//* a Anime image randomizer
-//* Stolen from Yorium (wew lad)
-//* because im baka
-//*
-//* Contributed by Capuccino
+/*
+ * yori.js - Random anime image command.
+ * 
+ * Contributed by Capuccino, Ovyerus.
+ */
 
-exports.commands = [
-    'moemoe',
-    'kyun' // this would be left undefined though, blame Dean
+const Promise = require('bluebird');
+const cheerio = require('cheerio');
+const request = require('request');
+
+const dirRegex = /.+\//;
+const imgRegex = /.+\.(?:png|jpg)/
+const baseUrl = 'http://i.yorium.moe/albums/';
+const ignore = [
+    'nsfw/',
+    'yuri/'
 ];
 
-// nya nya nya nya nya nya nya nya nya nya nya nya nya nya nya
-// nya nya nya nya nya nya nya nya nya nya nya nya nya nya nya
-const noud02 = require(`${__baseDir}/res/yori/fw.json`);
-// nya nya nya nya nya nya nya nya  nya nya nya nya nya nya nya
-// nya nya nya nya nya nya nya nya nya nya nya nya nya nya nya 
+exports.commands = [
+    'yori'
+];
 
-exports.moemoe = {
+exports.yori = {
     desc: 'idk about you but you have problems',
-    longDesc : 'dad teach me how to parse JSON pl0x',
-    main : (bot,ctx) => {
-        return new Promise((resolve,reject) => {
-            let url = noud02.link;
-            let fgt = url[Math.random() * url.length];
-
-            ctx.msg.channel.createMessage(fgt).then(() => resolve()).catch(err => ([err]));           
+    longDesc: 'dad teach me how to parse JSON pl0x',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            ctx.msg.channel.sendTyping();
+            request(baseUrl, (err, res, body) => {
+                if (err) {
+                    reject(err);
+                } else if (res.statusCode !== 200) {
+                    reject(new Error(`Invalid status code: ${res.statusCode}`));
+                } else {
+                    var $ = cheerio.load(body);
+                    var albums = $('a').text().trim().substring('Parent Directory'.length).trim().split(' ').filter(alb => {
+                        return dirRegex.test(alb);
+                    }).filter(alb => {
+                        return ignore.indexOf(alb) === -1;
+                    });
+                    var album = albums[Math.floor(Math.random() * albums.length)];
+                    request(baseUrl + album, (e, r, b) => {
+                        if (e) {
+                            reject(e);
+                        } else if (r.statusCode !== 200) {
+                            reject(new Error(`Invalid status code: ${r.statusCode}`));
+                        } else {
+                            $ = cheerio.load(b);
+                            var imgs = $('a').text().trim().substring('Parent Directory'.length).trim().split(' ').filter(i => {
+                                return imgRegex.test(i);
+                            });
+                            var img = imgs[Math.floor(Math.random() * albums.length)];
+                            ctx.msg.channel.createMessage(baseUrl + album + img).then(() => resolve()).catch(reject);
+                        }
+                    });
+                }
+            });
         });
     }
-};
-
+}
