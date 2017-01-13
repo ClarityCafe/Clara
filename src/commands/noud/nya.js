808e8b179cca8e650f0ce04fe6ce6a0818d7fa61
Ovyerus <iamovyerus@gmail.com>
2017-01-13 19:44:19 +1100

    fix cat command

--- src/commands/noud/nya.js
+++ src/commands/noud/nya.js
@@ -3,18 +3,20 @@
 // *
 // * contributed by Capuccino
 
+const Promise = require('bluebird');
+const cats  = require('cat-ascii-faces');
+
 exports.commands = [
     'cat'
 ];
-const cats  = require('cat-ascii-faces');
-
 
 exports.cat = {
     desc: 'prints out a random cat',
     longDesc: 'prints out a fucked up ASCII cat for your nya-ing pleasure',
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
-            ctx.msg.channel.sendMessage(cats()).then(() => resolve()).catch(err => ([err]));
+            var cat = cats();
+            ctx.msg.channel.createMessage(cat).then(() => resolve()).catch(err => ([err]));
         });
     }
 };
