098608e10f79dbcb8a0edf71bf44b5e812293d45
Capuccino <aguilarralph13@gmail.com>
2017-01-13 11:31:58 +0800

    ayy lmao

--- src/commands/mari/itsjoke.js
+++ src/commands/mari/itsjoke.js
@@ -12,14 +12,16 @@ exports.commands = [
     'itsjoke'
 ];
 
-
+const fs = require('fs');
+var files = fs.readdirSync('res/mari');
+var file = fs.readFileSync('directory' + files[Math.floor(Math.random() * files.length)]);
 exports.itsjoke = {
     desc: "it's Joke!",
     longDesc: "it's Joke!",
     usage: "<It's Joke!>",
     main: (bot, ctx) => {
         return new Promise((reject, resolve) => {
-            ctx.msg.channel.createMessage('',{file: require('fs').readFileSync('res/528.jpg'), name: '528.jpg'}).then(() => resolve()).catch(err => reject([err]));
+            ctx.msg.channel.createMessage('',{file: file , name:file.name }).then(() => resolve()).catch(err => reject([err]));
         });
     }
 };
