e03b26ef7e89553233137ede9d9106bebcc57a3e
Ovyerus <iamovyerus@gmail.com>
2017-01-17 16:57:37 +1100

    fix ibsearch

--- src/commands/ibsearch/ibsearch.js
+++ src/commands/ibsearch/ibsearch.js
@@ -8,9 +8,10 @@ const Promise = require('bluebird');
 const request = require('request');
 const color = require('dominant-color');
 const prettyBytes = require('pretty-bytes');
+const fs = require('fs');
 const ibKey = require(`${__baseDir}/config.json`).ibKey
 
-const defaultQuery = 'foxgirl';
+const defaultQuery = 'foxgirl kitsune nekomimi';
 const queryLimit = 75; // Default: 75
 
 exports.commands = [
@@ -29,7 +30,8 @@ exports.ibsearch = {
                     url: `https://ibsear.ch/api/v1/images.json?${defaultQuery ? `q=${encodeURIComponent(defaultQuery)}&` : ''}limit=${queryLimit}`,
                     method: 'GET',
                     headers: {
-                        'X-IbSearch-Key': ibKey
+                        'X-IbSearch-Key': ibKey,
+                        'User-Agent': 'owo-whats-this/0.1.1'
                     }
                 }, (err, res, body) => {
                     if (err) {
@@ -39,19 +41,28 @@ exports.ibsearch = {
                     } else {
                         var results = JSON.parse(body);
                         var item = results[Math.floor(Math.random() * results.length)];
-                        var imgURL = `https://${item.server}.ibsear.ch/resize/${item.path}?width=${Number(item.width) / 2}&height=${Number(item.height) / 2}`;
-                        color(imgURL, (err, color) => {
-                            ctx.msg.channel.createMessage({embed: {
-                                description: `**[Image Source](https://ibsear.ch/images/${item.id})**`,
-                                image: {url: imgURL},
-                                color: parseInt(color, 16),
-                                fields: [
-                                    {name: 'Image Information', value: `${item.width}x${item.height} ${prettyBytes(Number(item.size))}`},
-                                    {name: 'Tags', value: item.tags.replace(/_/g, '\_')}
-                                ],
-                                footer: {text: 'Time First Indexed by IbSearch'},
-                                timestamp: new Date(Number(item.found) * 1000)
-                            }}).then(() => resolve()).catch(err => reject([err]));
+                        var imgURL = `https://${item.server}.ibsear.ch/resize/${item.path}?width=${Number(item.width) > 1000 ? Number(item.width) / 2 : item.width}&height=${Number(item.width) > 1000 ? Number(item.height) / 2 : item.height}`;
+                        var saveName = item.path.split('/')[item.path.split('/').length - 1];
+                        request(imgURL).pipe(fs.createWriteStream(saveName)).on('close', () => {
+                            color(saveName, (err, color) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    fs.unlink(saveName, () => {
+                                        ctx.msg.channel.createMessage({embed: {
+                                            description: `**[Image Source](https://ibsear.ch/images/${item.id})**`,
+                                            image: {url: imgURL},
+                                            color: parseInt(color, 16),
+                                            fields: [
+                                                {name: 'Image Information', value: `${item.width}x${item.height} ${prettyBytes(Number(item.size))}`},
+                                                {name: 'Tags', value: item.tags.replace(/_/g, '\_')}
+                                            ],
+                                            footer: {text: 'Time First Indexed by IbSearch'},
+                                            timestamp: new Date(Number(item.found) * 1000)
+                                        }}).then(() => resolve()).catch(err => reject([err]));
+                                    });
+                                }
+                            });
                         });
                     }
                 });
@@ -62,7 +73,8 @@ exports.ibsearch = {
                     url: `https://ibsear.ch/api/v1/images.json?q=${query}&limit=${queryLimit}`,
                     method: 'GET',
                     headers: {
-                        'X-IbSearch-Key': ibKey
+                        'X-IbSearch-Key': ibKey,
+                        'User-Agent': 'owo-whats-this/0.1.1'
                     }
                 }, (err, res, body) => {
                     if (err) {
@@ -72,19 +84,28 @@ exports.ibsearch = {
                     } else {
                         var results = JSON.parse(body);
                         var item = results[Math.floor(Math.random() * results.length)];
-                        var imgURL = `https://${item.server}.ibsear.ch/resize/${item.path}?width=${Number(item.width) / 2}&height=${Number(item.height) / 2}`;
-                        color(imgURL, (err, color) => {
-                            ctx.msg.channel.createMessage({embed: {
-                                description: `**[Image Source](https://ibsear.ch/images/${item.id})**`,
-                                image: {url: imgURL},
-                                color: parseInt(color, 16),
-                                fields: [
-                                    {name: 'Image Information', value: `${item.width}x${item.height} ${prettyBytes(Number(item.size))}`},
-                                    {name: 'Tags', value: item.tags.replace(/_/g, '\_')}
-                                ],
-                                footer: {text: 'Time First Indexed by IbSearch'},
-                                timestamp: new Date(Number(item.found) * 1000)
-                            }}).then(() => resolve()).catch(err => reject([err]));
+                        var imgURL = `https://${item.server}.ibsear.ch/resize/${item.path}?width=${Number(item.width) > 1000 ? Number(item.width) / 2 : item.width}&height=${Number(item.width) > 1000 ? Number(item.height) / 2 : item.height}`;
+                        var saveName = item.path.split('/')[item.path.split('/').length - 1];
+                        request(imgURL).pipe(fs.createWriteStream(saveName)).on('close', () => {
+                            color(saveName, (err, color) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    fs.unlink(saveName, () => {
+                                        ctx.msg.channel.createMessage({embed: {
+                                            description: `**[Image Source](https://ibsear.ch/images/${item.id})**`,
+                                            image: {url: imgURL},
+                                            color: parseInt(color, 16),
+                                            fields: [
+                                                {name: 'Image Information', value: `${item.width}x${item.height} ${prettyBytes(Number(item.size))}`},
+                                                {name: 'Tags', value: item.tags.replace(/_/g, '\_')}
+                                            ],
+                                            footer: {text: 'Time First Indexed by IbSearch'},
+                                            timestamp: new Date(Number(item.found) * 1000)
+                                        }}).then(() => resolve()).catch(err => reject([err]));
+                                    });
+                                }
+                            });
                         });
                     }
                 });
