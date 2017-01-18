b7f022144fe40496a03c77f4f788d2bc12febfa6
Capuccino <aguilarralph13@gmail.com>
2017-01-18 08:25:53 +0800

    tfw EsLint

--- src/commands/osu/osu.js
+++ src/commands/osu/osu.js
@@ -34,7 +34,7 @@ function osuBlock(user) {
             {name: 'S Count', value: user.count_rank_s ? user.count_rank_s.replace(commaRegex, ',') : 'none', inline: true},
             {name: 'A Count', value: user.count_rank_a ? user.count_rank_a.replace(commaRegex, ',') : 'none', inline: true}
         ]
-    }}
+    }};
 }
 
 exports.commands = [
@@ -129,7 +129,7 @@ exports.mania = {
             }
         });
     }
-}
+};
 
 exports.taiko = {
     desc: "Retrive stats for osu!'s Taiko mode for a user.",
@@ -158,4 +158,4 @@ exports.taiko = {
             }
         });
     }
-}
+};
