ee8345a4087a3fb2cec95782c2e67e8efbaf857d
sr229 <aguilarralph13@gmail.com>
2017-01-20 20:48:49 +0800

    Promisify Scratchpad

--- src/commands/scratchpad_command/scratchpad.js
+++ src/commands/scratchpad_command/scratchpad.js
@@ -5,25 +5,29 @@
 // * by Capuccino
 
 exports.commands = [
-    'scratch' 
+    'scratch'
 ];
 
 const chatbot = require('datchatbot');
 const ayana = new chatbot.Client('', 'kotori');
+const Promise = require('bluebird');
 
 exports.scratch = {
     desc: 'Experimental Module. Bot Creators only',
     longDesc: 'This is an experimental module. Bot Creators only',
-    usage : '<message>',
-    main: (bot,ctx) => {
-        //we're reusing the same condition if the suffix length is 0
-        if (ctx.suffix.length === 0) {
-            ctx.msg.channel.createMessage('Hm?').then(()=> resolve()).catch(err => ([err]));
-        } else {
-            ayana.talk(ctx.suffix).then(()=> {
-                //TODO: find out how to query a response from the chatbot
-                ctx.msg.channel.createMessage(/* left empty since we don't know how to query the response*/).then(()=> resolve).catch(err => ([err]));
-            }).catch(err => ([err]));
-        }
+    usage: '<message>',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            // we're reusing the CB.io coding if ctx length is 0
+            if (ctx.msg.suffix.length === 0) {
+                ctx.msg.channel.createMessage('Hi!').then(() => {
+                    reject([new Error('No Message provided!')]);
+                }).catch(err => ([err]));
+            } else {
+                ayana.ask(ctx.suffix).then(() => {
+                    ctx.msg.channel.createMessage( /* left empty because there's no function to do responses */ ).then(() => resolve()).catch(err => ([err]));
+                }).catch(err => ([err]));
+            }
+        });
     }
 };
