ad8cfa8a1b8730a686e5c28ec7f9ebc7dffac966
sr229 <aguilarralph13@gmail.com>
2017-02-01 21:14:39 +0800

    made embed object inline

--- src/commands/tsundereloli/weather.js
+++ src/commands/tsundereloli/weather.js
@@ -9,24 +9,6 @@ exports.commands = [
 const Promise = require('bluebird');
 const weather = require('weather-js'); // npm package is https://www.npmjs.com/package/weather-js
 
-function weatherBlock(location, current, forecast) {
-    return {
-        embed: {
-            title: `Weather Information for ${location.name}.`,
-            description: current.skytext,
-            color: 0xFD7BB5,
-            thumbnail: location.imagerelativeurl,
-            fields: [
-                {name: 'Humidity', value: current.humidity, inline: true},
-                {name: 'Temprature', value: current.temperature, inline: true},
-                {name: 'Wind Speed', value: current.winddisplay, inline: true},
-                {name: 'Observed From', value: current.observationpoint, inline: true}
-
-            ],
-            footer: {text: `Queried from MSN Weather. Last Updated by ${forecast.date} , ${forecast.day}`}
-        }
-    };
-}
 exports.weather = {
     desc: 'Check for weather on a city',
     usage: '<City, ie. Melbourne, Australia>',
@@ -38,7 +20,16 @@ exports.weather = {
                 weather.find({search : ctx.suffix, degreeType: 'C'}, (err, result) => {
                     let loli = JSON.parse(result);
                     ctx.msg.channel.createMessage({embed : {
-                        title: `Weather information for ${loli.location.name}`
+                        title: `Weather information for ${loli.location.name}`,
+                        description : loli.current.skytext,
+                        color : 0xFD7BB5,
+                        thumbnail: loli.location.imagerelativeurl,
+                        fields : [
+                            {name: 'Temperature', value : loli.current.temperature, inline : true},
+                            {name: 'Humidity' , value: loli.current.humidity, inline: true},
+                            {name: 'Wind Speed', value: loli.current.winddisplay, inline:true}
+                        ],
+                        footer : {text: `Observed from ${loli.current.observationpoint}. Updated as of ${loli.forecast.day}, ${loli.forecast.date}.`}
                     }}).then(() => resolve()).catch(err => ([err]));
                 });
             }
