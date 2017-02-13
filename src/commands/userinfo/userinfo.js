57838a0ef51a3918e245fed49c5ad6c73d5b4615
Capuccino <aguilarralph13@gmail.com>
2017-02-13 05:11:25 +0000

    WORKAROUND: used a fixed colour for userinfo

--- src/commands/userinfo/userinfo.js
+++ src/commands/userinfo/userinfo.js
@@ -19,7 +19,7 @@ function infoBlock(member, roles, color) {
         author: {name: utils.formatUsername(member.user), icon_url: member.bot ? 'https://cdn.discordapp.com/emojis/230105988211015680.png' : ''},
         description: `**[Full Avatar](${avatar.replace('size=256', 'size=1024')})**`,
         thumbnail: {url: avatar, width: 128, height: 128},
-        color: color,
+        color: 0xFD7BB5,
         fields: [
             {name: 'Nickname', value: member.nick || 'None', inline: true}, 
             {name: 'ID', value: member.id, inline: true},
