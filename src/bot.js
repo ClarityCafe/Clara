1e22896301916452a7eee75261631fdd681537ff
Capuccino <chinodesuuu@gmail.com>
2017-02-23 06:13:04 +0000

    fix w?

--- src/bot.js
+++ src/bot.js
@@ -164,7 +164,7 @@ bot.on('ready', () => {
     } else {
         logger.info('Reconnected to Discord from disconnect.');
     }
-    bot.editStatus('online', { name: `${config.gameName || }`, type: `${config.gameType || 0}`, url: `${config.gameURL || null}` });
+    bot.editStatus('online', { name: `${config.gameName || `with ${bot.guilds.size} servers.`}`, type: `${config.gameType || 0}`, url: `${config.gameURL || null}` });
 });
 
 bot.on('shardReady', shard => {
@@ -176,11 +176,11 @@ bot.on('shardResume', shard => {
 });
 
 bot.on('guildCreate', () => {
-    bot.editStatus('online', { name: `${config.gameName || ''}`, type: `${config.gameType || 0}`, url: `${config.gameURL || null}` });
+    bot.editStatus('online', { name: `${config.gameName || `with ${bot.guilds.size} servers.`}`, type: `${config.gameType || 0}`, url: `${config.gameURL || null}` });
 });
 
 bot.on('guildDelete', () => {
-    bot.editStatus('online', { name: `${config.gameName || ''}`, type: `${config.gameType || 0}`, url: `${config.gameURL || null}` });
+    bot.editStatus('online', { name: `${config.gameName || `with ${bot.guilds.size} servers.`}`, type: `${config.gameType || 0}`, url: `${config.gameURL || null}` });
 
 });
 
