eb22e42d88bc6c4670dd5d09459c00bb5b944219
Ovyerus <iamovyerus@gmail.com>
2017-02-26 12:20:40 +1100

    Clean up poi and escape tableflip to not trigger bots.

--- src/commands/poi/poi.js
+++ src/commands/poi/poi.js
@@ -1,28 +1,27 @@
-// * Poi
-// * Poi?
-// * POI!!!!!???
-// * [Poi intensifies]
-// * -Capuccino
+/*
+ * poi.js - ¯\_(ツ)_/¯
+ * 
+ * Contributed by Capuccino and Ovyerus
+ */
 
 exports.commands = [
     'poi'
 ];
 
+const responses = [
+    'P-Poi?',
+    'Poi...',
+    'POI!!!!!?',
+    'P-Poi..',
+    'Poi uwu',
+    '\u200b(\u200b╯\u200b°\u200b□\u200b°\u200b）\u200b╯\u200b︵\u200b \u200b┻\u200b━\u200b┻\u200b'
+];
+
 exports.poi = {
     desc: 'Poi? POI!? POOOOOOOOOOIIIIIIII!!!!',
     main: (bot,ctx) => {
-        let responses = [
-            'P-Poi?',
-            'Poi...',
-            'POI!!!!!?',
-            'P-Poi..',
-            'Poi uwu',
-            '(╯°□°）╯︵ ┻━┻'
-            ];
         return new Promise((resolve, reject) => {
-            ctx.msg.channel.createMessage(responses[Math.floor(Math.random()* responses.length)])
-                .then(resolve)
-                .catch(reject);
+            ctx.msg.channel.createMessage(responses[Math.floor(Math.random() * responses.length)]).then(resolve).catch(reject);
         });
     }
 };
