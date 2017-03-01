b914b7c944a00761d01af74a2672e2f20bb32094
Ovyerus <iamovyerus@gmail.com>
2017-03-01 20:23:20 +1100

    uuuhhh

---
+++ src/commands/osu/osu.js
@@ -0,0 +1,137 @@
+/*
+ * osu.js - osu! stats command.
+ * 
+ * Contributed by Capuccino and Ovyerus.
+ */
+
+const config = require(`${__baseDir}/config.json`);
+const osu = require('osu')(config.osuApiKey);
+const iso3166 = require('iso-3166-1-alpha-2');
+
+var commaRegex = /\B(?=(\d{3})+(?!\d))/g; // Thank you Brayzure uwu
+
+function osuBlock(user) {
+    return {embed: {
+        title: `osu! stats for **${user.username}**`,
+        url: `https://osu.ppy.sh/u/${user.user_id}`,
+        thumbnail: {url: `http://a.ppy.sh/${user.user_id}`},
+        color: 0xFD7BB5,
+        fields: [
+            {name: 'ID', value: user.user_id, inline: true},
+            {name: 'Country', value: `${iso3166.getCountry(user.country)} :flag_${user.country.toLowerCase()}:`, inline: true},
+            {name: 'Country Rank', value: user.pp_country_rank.replace(commaRegex, ','), inline: true},
+            {name: 'Beatmaps Played', value: user.playcount ? user.playcount.replace(commaRegex, ',') : 'none', inline: true},
+            {name: '300s Scored', value: user.count300 ? user.count300.replace(commaRegex, ',') : 'none', inline: true},
+            {name: '100s Scored', value: user.count100 ? user.count100.replace(commaRegex, ',') : 'none', inline: true},
+            {name: '50s Scored', value: user.count50 ? user.count50.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'Ranked Score', value: user.ranked_score ? user.ranked_score.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'Total Score', value: user.total_score ? user.total_score.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'PP', value: user.pp_raw ? user.pp_raw.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'Level', value: user.level ? Number(user.level).toFixed(1).toString().replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'Rank', value: user.pp_rank ? user.pp_rank.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'Accuracy', value: user.accuracy ? Number(user.accuracy).toFixed(2).toString('utf8') : 'none', inline: true},
+            {name: 'SS Count', value: user.count_rank_ss ? user.count_rank_ss.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'S Count', value: user.count_rank_s ? user.count_rank_s.replace(commaRegex, ',') : 'none', inline: true},
+            {name: 'A Count', value: user.count_rank_a ? user.count_rank_a.replace(commaRegex, ',') : 'none', inline: true}
+        ]
+    }};
+}
+
+exports.commands = [
+    'osu',
+    'mania',
+    'taiko',
+    'ctb'
+];
+
+exports.osu = {
+    desc: "Retrive stats for osu!'s standard mode for a user.",
+    fullDesc: "Uses the osu! API to get information for osu!'s standard mode for a user.",
+    usage: '<osu! username/id>',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            if (ctx.suffix.length === 0) {
+                ctx.msg.channel.createMessage('Please give me a name to search with.').then(resolve).catch(reject);
+            } else {
+                ctx.msg.channel.sendTyping();
+                osu.get_user({u: ctx.suffix}, res => {
+                    var user = res[0];
+                    if (!user || !user.user_id) {
+                        ctx.msg.channel.createMessage('A user with that name or ID could not be found.').then(resolve).catch(reject);
+                    } else {
+                        ctx.msg.channel.createMessage(osuBlock(user)).then(resolve).catch(reject);
+                    }
+                });
+            }
+        });
+    }
+};
+
+exports.ctb = {
+    desc: "Retrive stats for osu!'s Catch The Beat mode for a user.",
+    fullDesc: "Uses the osu! API to get information for osu!'s Catch The Beat mode for a user.",
+    usage: '<osu! username/id>',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            if (ctx.suffix.length === 0) {
+                ctx.msg.channel.createMessage('Please give me a name to search with.').then(resolve).catch(reject);
+            } else {
+                ctx.msg.channel.sendTyping();
+                osu.get_user({u: ctx.suffix, m: 2}, res => {
+                    var user = res[0];
+                    if (!user || !user.user_id) {
+                        ctx.msg.channel.createMessage('A user with that name or ID could not be found.').then(resolve).catch(reject);
+                    } else {
+                        ctx.msg.channel.createMessage(osuBlock(user)).then(resolve).catch(reject);
+                    }
+                });
+            }
+        });
+    }
+};
+
+exports.mania = {
+    desc: "Retrive stats for osu!'s osu!mania mode for a user.",
+    fullDesc: "Uses the osu! API to get information for osu!'s osu!mania mode for a user.",
+    usage: '<osu! username/id>',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            if (ctx.suffix.length === 0) {
+                ctx.msg.channel.createMessage('Please give me a name to search with.').then(resolve).catch(reject);
+            } else {
+                ctx.msg.channel.sendTyping();
+                osu.get_user({u: ctx.suffix, m: 3}, res => {
+                    var user = res[0];
+                    if (!user || !user.user_id) {
+                        ctx.msg.channel.createMessage('A user with that name or ID could not be found.').then(resolve).catch(reject);
+                    } else {
+                        ctx.msg.channel.createMessage(osuBlock(user)).then(resolve).catch(reject);
+                    }
+                });
+            }
+        });
+    }
+};
+
+exports.taiko = {
+    desc: "Retrive stats for osu!'s Taiko mode for a user.",
+    fullDesc: "Uses the osu! API to get information for osu!'s Taiko mode for a user.",
+    usage: '<osu! username/id>',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            if (ctx.suffix.length === 0) {
+                ctx.msg.channel.createMessage('Please give me a name to search with.').then(resolve).catch(reject);
+            } else {
+                ctx.msg.channel.sendTyping();
+                osu.get_user({u: ctx.suffix, m: 1}, res => {
+                    var user = res[0];
+                    if (!user || !user.user_id) {
+                        ctx.msg.channel.createMessage('A user with that name or ID could not be found.').then(resolve).catch(reject);
+                    } else {
+                        ctx.msg.channel.createMessage(osuBlock(user)).then(resolve).catch(reject);
+                    }
+                });
+            }
+        });
+    }
+};
---
+++ src/commands/ship/ship.js
@@ -0,0 +1,37 @@
+/*
+ * Ship Command
+ * 
+ * Blame Kawaiibot
+ * 
+ * Contributed by Capuccino
+ */
+
+exports.commands = [
+    'ship'
+];
+
+exports.ship = {
+    desc: 'basically a ship command.',
+    longDesc: 'Happy Shipping!',
+    usage: '<user Mention (Maximum 2)>',
+    main: (bot, ctx) => {
+        return new Promise((resolve,reject) => {
+            if(ctx.msg.mentions.length < 2) {
+                ctx.msg.channel.createMessage('Specify at least two users').then(() => reject(new Error('No User Specified'))).catch(reject);
+            } else {
+                //placeholder lets for now
+                let user1 = ctx.msg.mentions[1];
+                let user2 = ctx.msg.mentions[2];
+                let result = user1.username.substring(0, Math.floor(user1.username.length / 2)) + user2.username.substring(Math.floor(user2.username.length / 2));
+                ctx.msg.channel.createMessage({embed : {
+                    title: 'Happy Shipping!',
+                    color: 0xFD7BB5,
+                    fields : [
+                        {name: 'Your Ship name is', value: result, inline: true}
+                    ],
+                    footer: "We are not solely responsible for anyone's broken heart resulting from your ship."
+                }}).then(() => resolve).catch(reject);
+            }
+        });
+    }
+};
