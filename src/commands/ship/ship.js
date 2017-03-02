981c500e1d4d99cb270020db2f56d4913233cfbb
Capuccino <chinodesuuu@gmail.com>
2017-03-02 09:22:42 +0000

    made ship accept regular strings

--- src/commands/ship/ship.js
+++ src/commands/ship/ship.js
@@ -18,9 +18,9 @@ exports.ship = {
             if (ctx.msg.mentions.length > 2 || ctx.args.length > 2) {
                 ctx.msg.channel.createMessage('Give me at least two names').then(() => reject(new Error('Now you fucked up.'))).catch(reject);
             } else {
-                let senpai = ctx.msg.mentions[0] ? ctx.msg.mentions[0].username : ctx.args[0];
-                let kouhai = ctx.msg.mentions[1] ? ctx.msg.mentions[1].username : ctx.args[1];
-                let result = senpai.substring(0, Math.floor(senpai.length / 2)) + kouhai.substring(Math.floor(kouhai.length / 2));
+                let a = ctx.msg.mentions[0] ? ctx.msg.mentions[0].username : ctx.args[0];
+                let b = ctx.msg.mentions[1] ? ctx.msg.mentions[1].username : ctx.args[1];
+                let result = a.substring(0, Math.floor(a.length / 2)) + b.substring(Math.floor(b.length / 2));
                 ctx.msg.channel.createMessage(`Happy Shipping!\n Your Ship name is : **${result}**!\n _Disclaimer: We are not responsible if you get attacked by a Yandere prior from this._`).then(() => resolve).catch(reject);
             }
         });
