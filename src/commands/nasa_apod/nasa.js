a7c4178d1ccd1bac339d6a4af678739ae8d747ca
sr229 <aguilarralph13@gmail.com>
2017-03-03 22:57:33 +0800

    try to catch err to prevent bot from crashing

--- src/commands/nasa_apod/nasa.js
+++ src/commands/nasa_apod/nasa.js
@@ -8,32 +8,37 @@ exports.commands = [
 ];
 
 const voyager = require('request');
+const apiKey= 'NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo';
 
 exports.apod = {
     desc: 'returns the latest news from NASA',
     main: (bot,ctx) => {
         return new Promise((resolve,reject) => {
-            voyager('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo', (err ,res ,body) => {
+            voyager(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, (err ,res ,body) => {
                 if(err) {
                     reject(err);
                 } else if (res.statusCode !== 200) {
                     ctx.msg.channel(`Oh my!, something unexpected happened! Try Again! (Error Code ${res.statusCode})`).then (() => {
                         reject( new Error(`Invalid Response! Expected JSON Response, got code ${res.statusCode} instead.`));
                     }).catch(err => (err));
+                } else if (err){
+                    reject;
                 } else {
                     let pioneer = JSON.stringify(body);
                     ctx.msg.channel.createMessage({embed : {
                         title: pioneer.title,
-                        thumbnail:{url: 'https://api.nasa.gov/images/logo.png', width: 150, height : 150},
+                        thumbnail:{url: 'https://api.nasa.gov/images/logo.png', width: 10, height : 10},
                         color: 0xFD7BB5 ,//placeholder,
+                        image: {
+                            url: pioneer.hdurl
+                        },
                         fields : [
-                            {name: 'image', value: pioneer.hdurl},
                             {name: '', value: pioneer.explanation}
                         ],
                         footer : {text: `Retrieved using NASA Open API. Retrieved ${JSON.parse(body.date)}.`}
                     }}).then(() => resolve).catch(err => err);
-                }
-            })
-        })
+                } 
+            });
+        });
     }
 };
