e6bee9190ef285f962b1f3e5d859fc9e9bc23342
Ovyerus <iamovyerus@gmail.com>
2017-03-07 18:38:11 +1100

    Indentssss

--- src/lib/prefixParser.js
+++ src/lib/prefixParser.js
@@ -14,27 +14,29 @@ const bot = require(`${__baseDir}/bot.js`).bot;
  */
 function parse(content) {
     return new Promise((resolve, reject) => {
-        if (typeof content !== 'string') reject(new Error('content is not a string'));
-
-        var oldContent = content;
-        var prefixes = JSON.parse(fs.readFileSync(`${__baseDir}/data/prefixes.json`));
-        prefixes.push(`<@${bot.user.id}> `);
+        if (typeof content !== 'string') {
+            reject(new Error('content is not a string'));
+        } else {
+            var oldContent = content;
+            var prefixes = JSON.parse(fs.readFileSync(`${__baseDir}/data/prefixes.json`));
+            prefixes.push(`<@${bot.user.id}> `);
         
-        if (!content.startsWith(bot.config.mainPrefix)) {
-            for (let i in prefixes) {
-                if (content.startsWith(prefixes[i])) {
-                    content = content.substring(prefixes[i].length);
-                    break;
+            if (!content.startsWith(bot.config.mainPrefix)) {
+                for (let i in prefixes) {
+                    if (content.startsWith(prefixes[i])) {
+                        content = content.substring(prefixes[i].length);
+                        break;
+                    }
                 }
+            } else {
+                content = content.substring(bot.config.mainPrefix.length);
             }
-        } else {
-            content = content.substring(bot.config.mainPrefix.length);
-        }
 
-        if (content !== oldContent) {
-            resolve(content);
-        } else {
-            resolve();
+            if (content !== oldContent) {
+                resolve(content);
+            } else {
+                resolve();
+            }
         }
     });
 }
