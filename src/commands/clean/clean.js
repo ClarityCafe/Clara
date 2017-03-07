2775ca6bf28c6b3ec0deb0ce8c4f682de470cca4
Ovyerus <iamovyerus@gmail.com>
2017-03-07 17:31:58 +1100

    Clean additions and send message amount

--- src/commands/clean/clean.js
+++ src/commands/clean/clean.js
@@ -11,12 +11,13 @@ exports.commands = [
 exports.clean = {
     desc: 'clean messages created by the bot itself',
     main: (bot, ctx) => {
-        return new Promise((resolve,reject) => {
+        return new Promise((resolve, reject) => {
             ctx.msg.channel.getMessages(100).then(msgs => {
                 let delet = [];
+                msgs = msgs.filter(m => m.author.id === bot.user.id);
                 msgs.forEach(m => delet.push(m.delete()));
                 return Promise.all(delet);
-            });
+            }).then(amt => ctx.msg.channel.createMessage(amt)).then(resolve).catch(reject);
         });
     }
 };
