04ef975733300e35c7634d58db1db88a2dd194de
Capuccino <aguilarralph13@gmail.com>
2017-03-10 13:45:06 +0800

    I had a dream i can understand Programs and RegEXP

--- src/commands/currency_checker/currency.js
+++ src/commands/currency_checker/currency.js
@@ -12,6 +12,7 @@ exports.commands = [
 ];
 
 const fx = require('./money.min');
+var awauRegex = str => /^\d+(?:\.\d+)? (?:[A-Z]{3}\s?){2}$/.test(str);
 
 exports.convert = {
     desc: 'converts money, that it',
@@ -22,6 +23,8 @@ exports.convert = {
                 ctx.msg.channel.createMessage('Give me a number then two currencies first.').then(resolve).catch(reject);
             } else if (ctx.args > 4) {
                 ctx.msg.channel.createMessage('Insufficient information. I need a number and at least two currencies').then(reject).catch(reject);
+            }else if (!ctx.args.test(awauRegex)){
+                ctx.msg.channel.createMessage('Nope, wrong format!').then(resolve).then(reject);
             } else {
                 fx(ctx.args[1]).from(ctx.args[2]).to(ctx.args[3]);
             }
