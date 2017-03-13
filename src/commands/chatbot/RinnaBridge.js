133d46b4fb43604afcc22ad457049c51bf49a7f9
Capuccino <chinodesuuu@gmail.com>
2017-03-13 04:16:20 -0400

    addded RinnaBridger
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

---
+++ src/commands/chatbot/RinnaBridge.js
@@ -0,0 +1,14 @@
+/* Bridge for Rinna using Twitter
+*
+*
+* Contributed by Capuccino
+*/
+
+const Twitter = require('twitter');
+const RinnaBridge = new Twitter({
+	consumer_key: '',
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secret: ''
+});
+
