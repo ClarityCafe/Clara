34daf8da732a01f458f8fa94ca0b224cc76d0ebd
Capuccino <aguilarralph13@gmail.com>
2017-03-15 07:50:14 +0800

    awau

--- src/commands/nadeko/nadeko.js
+++ src/commands/nadeko/nadeko.js
@@ -15,9 +15,9 @@ const emotes = [
 exports.nadeko = {
     desc: 'who did it?',
     longDesc: 'owo whats this?',
-    main: (bot , ctx) => {
-        return new Promise ((resolve, reject) => {           
-            ctx.msg.channel.createMessage(localeManager.t('nadeko', settings.locale, {user : ctx.msg.author.mention, emote: emotes[Math.floor(Math.random() * emotes.length)]})).then(resolve).catch(reject);
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {           
+            ctx.msg.channel.createMessage(localeManager.t('nadeko', settings.locale, {user: ctx.msg.author.mention, emote: emotes[Math.floor(Math.random() * emotes.length)]})).then(resolve).catch(reject);
         });
     }
 };
