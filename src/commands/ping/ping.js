d1ae52f07ada1558356c5acfa7f99db94ce9f76f
Ovyerus <iamovyerus@gmail.com>
2017-03-15 17:44:04 +1100

    fix ping

--- src/commands/ping/ping.js
+++ src/commands/ping/ping.js
@@ -15,8 +15,8 @@ exports.ping = {
     fullDesc: "Ping the bot and check it's latency.",
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
-            ctx.msg.channel.createMessage(localeManager.t('pong', settings.locale)).then(m => {
-                return m.edit(localeManager.t('pong', settings.locale) + ` \`${m.timestamp - ctx.msg.timestamp}ms\``);
+            ctx.msg.channel.createMessage(localeManager.t('pong', ctx.settings.locale)).then(m => {
+                return m.edit(localeManager.t('pong', ctx.settings.locale) + ` \`${m.timestamp - ctx.msg.timestamp}ms\``);
             }).then(resolve).catch(reject);
         });
     }
