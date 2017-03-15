797110a26d1ef3a2831c6890e67012a3ea254e63
Capuccino <aguilarralph13@gmail.com>
2017-03-15 11:49:16 +0800

    fixed more trailing whitespaces

--- src/commands/ship/ship.js
+++ src/commands/ship/ship.js
@@ -1,8 +1,8 @@
 /*
  * Ship Command
- * 
+ *
  * Blame Kawaiibot
- * 
+ *
  * Contributed by Capuccino
  */
 
@@ -19,7 +19,7 @@ exports.ship = {
                 ctx.msg.channel.createMessage(localeManager.t('ship-noArgs', settings.locale)).then(resolve).catch(reject);
             } else {
                 let a = ctx.msg.mentions[0] === undefined ? ctx.args[0] : ctx.msg.mentions[0].username;
-                let b = ctx.msg.mentions[1] === undefined ? ctx.args[1] : ctx.msg.mentions[1].username; 
+                let b = ctx.msg.mentions[1] === undefined ? ctx.args[1] : ctx.msg.mentions[1].username;
                 let result = a.substring(0, Math.floor(a.length / 2)) + b.substring(Math.floor(b.length / 2));
 
                 ctx.msg.channel.createMessage(localeManager.t('ship', settings.locale, {result})).then(() => resolve).catch(reject);
