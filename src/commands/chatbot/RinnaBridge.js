70c233d9c050fff08a74a293d2346f11137ee9e1
Capuccino <aguilarralph13@gmail.com>
2017-03-15 08:30:49 +0800

    thonk

--- src/commands/chatbot/RinnaBridge.js
+++ src/commands/chatbot/RinnaBridge.js
@@ -8,6 +8,7 @@ const Twitter = require('twitter');
 
 /**
  * Initializes a new Twitter Client for DM Bridge.
+ * @prop {Object} options everythonk for auth
  * @prop {string} consumerKey your consumer key for twitter.
  * @prop {string} consumerSecret your consumer secret for twitter.
  * @prop {string} accessKey your access key for twitter.
@@ -18,7 +19,7 @@ class RinnaClient extends Twitter {
     constructor(options) {
         super(options);
         this.consumerkey = options.consumerKey;
-        this.consumersecret = options.consumerSecret;
+        this.consumerSecret = options.consumerSecret;
         this.accessKey = options.accessKey;
         this.accessSecret = options.accessSecret;
     }
