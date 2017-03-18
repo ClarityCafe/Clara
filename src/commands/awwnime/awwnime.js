0f12461928ba220821fe9014eaf52434c79bef3e
sr229 <aguilarralph13@gmail.com>
2017-03-18 16:09:24 +0800

    forgot to define awau

--- src/commands/awwnime/awwnime.js
+++ src/commands/awwnime/awwnime.js
@@ -16,12 +16,12 @@ exports.awwnime = {
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
             if (!ctx.args) {
-                randomAnimeWallpapers().then(images => {
+                anime.randomAnimeWallpapers().then(images => {
                     let animu = images[Math.floor(Math.random()*Object.id.length)];
                     ctx.msg.channel.createMessage('', {file: animu.full, name: animu.id}).then(resolve).catch(reject);
                 }).catch(reject);
             } else {
-                randomAnimeWallpapers(ctx.args[2]).then(images => {
+                anime.randomAnimeWallpapers(ctx.args[2]).then(images => {
                     ctx.msg.channel.createMessage({embed: {
                         title: `Search Results for ${ctx.args[2]}`,
                         image: images.full
