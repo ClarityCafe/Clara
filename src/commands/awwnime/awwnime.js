2385086bdec2fa58d61352fd7194cef77ffd5cbf
sr229 <aguilarralph13@gmail.com>
2017-03-18 16:04:07 +0800

    anyawau

--- src/commands/awwnime/awwnime.js
+++ src/commands/awwnime/awwnime.js
@@ -15,11 +15,18 @@ exports.awwnime = {
     desc: 'Gets you a random anime picture outside of yorium.moe',
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
-            if(!ctx.args) {
+            if (!ctx.args) {
                 randomAnimeWallpapers().then(images => {
                     let animu = images[Math.floor(Math.random()*Object.id.length)];
                     ctx.msg.channel.createMessage('', {file: animu.full, name: animu.id}).then(resolve).catch(reject);
                 }).catch(reject);
+            } else {
+                randomAnimeWallpapers(ctx.args[2]).then(images => {
+                    ctx.msg.channel.createMessage({embed: {
+                        title: `Search Results for ${ctx.args[2]}`,
+                        image: images.full
+                    }}).then(resolve).catch(reject);
+                })
             }
         });
     }
