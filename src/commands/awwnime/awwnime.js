222802f3b6027098cd2e6b025c8e48244fd2b198
sr229 <aguilarralph13@gmail.com>
2017-03-18 15:55:09 +0800

    Awwnime af

---
+++ src/commands/awwnime/awwnime.js
@@ -0,0 +1,26 @@
+/* 
+ *  awwnime - Generates a random anime picture.
+ * 
+ * 
+ *  Contributed by Capuccino
+ */
+
+exports.commands = [
+    'awwnime'
+];
+
+const anime = require('random-anime-wallpapers');
+
+exports.awwnime = {
+    desc: 'Gets you a random anime picture outside of yorium.moe',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            if(!ctx.args) {
+                randomAnimeWallpapers().then(images => {
+                    let animu = images[Math.floor(Math.random()*Object.id.length)];
+                    ctx.msg.channel.createMessage('',{file: animu.image, name: animu.id}).then(resolve).catch(reject);
+                }).catch(reject);
+            }
+        });
+    }
+};
