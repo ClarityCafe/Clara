7d050bd714bf58c71a2df1e8f3b552b28dc02a4f
sr229 <aguilarralph13@gmail.com>
2017-03-20 22:01:12 +0800

    can't we all just get along AND JUST USE ONE FUCKING LANG!

--- src/commands/about/about.js
+++ src/commands/about/about.js
@@ -18,8 +18,7 @@ exports.about = {
             let version = JSON.parse(fs.readFileSync(`${__baseDir}/package.json`)).version;
             ctx.msg.channel.createMessage({embed: {
                 title: 'About Clara',
-                description: `Clara is a Open Source Project by Ovyerus and Capuccino.\nThe Discord bot and it's source code is licensed under MIT.\n\n
-                [GitHub Repository](https://github.com/awau/Clara) | [Donate!](https://patreon.com/capuccino)`,
+                description: localeManager.t('about-nya', ctx.settings.locale),
                 image: {url: 'https://github.com/awau/Clara/raw/master/nodebot_logo.png'},
                 footer: {text: `${bot.user.username} uses Clara ${version}`}
 
