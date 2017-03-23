4e02e5fb948353f79fffe27c44fada63617fbe22
Ovyerus <iamovyerus@gmail.com>
2017-03-23 20:19:47 +1100

    Rename and merge a couple events

--- src/events/shardDisconnect.js
+++
@@ -1,5 +0,0 @@
-module.exports = bot => {
-    bot.on('shardDisconnect', (err, shard) => {
-        if (err) logger.customError('shard/shardStatus', `Shard ${shard} disconnected. Reason ${err}`);
-    });
-};
--- src/events/shardResume.js
+++
@@ -1,5 +0,0 @@
-module.exports = bot => {
-    bot.on('shardResume', shard => {
-        logger.custom('blue', 'shard/shardInfo', `Shard ${shard} has resumed.`);
-    });
-};
