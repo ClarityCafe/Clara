4b0c6a0bf6ef65401f9a68ed847b24edd1b2b6c5
Ovyerus <iamovyerus@gmail.com>
2017-03-24 18:28:22 +1100

    Holy mother of shitty indents

--- src/events/guilds.js
+++ src/events/guilds.js
@@ -1,14 +1,12 @@
 const utils = require(`${__baseDir}/modules/utils`);
 
 module.exports = bot => {
-        bot.on('guildCreate', g => {
-                    if (g.members.filter(m => m.bot).size / g.members.size >= 0.75) {
-                        logger.info(`Leaving bot collection '${g.name}' (${g.id})`);
-                        g.leave();
-                    }
-                    else {
-                        bot.editStatus('online', {
-                                    name: `${bot.config.gameName || `${bot.config.mainPrefix}help for commands!`} | ${bot.guilds.size} ${bot.guilds.size === 1 ? 'server' : 'servers'}`, type: bot.config.gameURL ? 1 : 0, url: `${bot.config.gameURL || null}`});
+    bot.on('guildCreate', g => {
+        if (g.members.filter(m => m.bot).size / g.members.size >= 0.75) {
+            logger.info(`Leaving bot collection '${g.name}' (${g.id})`);
+            g.leave();
+        } else {
+            bot.editStatus('online', {name: `${bot.config.gameName || `${bot.config.mainPrefix}help for commands!`} | ${bot.guilds.size} ${bot.guilds.size === 1 ? 'server' : 'servers'}`, type: bot.config.gameURL ? 1 : 0, url: `${bot.config.gameURL || null}`});
             bot.postGuildCount();
         }
     });
@@ -30,14 +28,15 @@ module.exports = bot => {
             }
         });
     });
+
     bot.on('guildMemberDelete', (g, m) => {
         bot.guildSettings(g.id).then(res => {
-              if(!res || !res.greeting || !res.greeting.enabled || !res.greeting.channelID || !res.parting.message) {
-           return null;
-       } else {
-           let msg = res.parting.message.replace(/\{\user\}\}/g, m.mention).replace(/\{name\}\}/g, utils.formatUsername(m));
-           return g.channels.get(res.greeting.channelID).createMessage(msg);
-       }
+            if (!res || !res.greeting || !res.greeting.enabled || !res.greeting.channelID || !res.parting.message) {
+                return null;
+            } else {
+                let msg = res.parting.message.replace(/\{\user\}\}/g, m.mention).replace(/\{name\}\}/g, utils.formatUsername(m));
+                return g.channels.get(res.greeting.channelID).createMessage(msg);
+            }
         });
     });
 };
