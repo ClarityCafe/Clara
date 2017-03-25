eb3171c0704bc1f128db94acdfccafc2bd44c656
sr229 <aguilarralph13@gmail.com>
2017-03-25 12:04:23 +0800

    Promisisfy random.cat

--- src/commands/nyaa/azusa.js
+++ src/commands/nyaa/azusa.js
@@ -4,7 +4,7 @@
  * Contributed by Capuccino
  */
 
-const request = require('request');
+const got = require('got');
 
 exports.commands = [
     'nyaa'
@@ -15,16 +15,10 @@ exports.nyaa = {
     fullDesc: 'Gets an image from random.cat',
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
-            request('http://random.cat/meow', (err, res, body) => {
-                if (err) {
-                    reject(err);
-                } else if (res.statusCode !== 200) {
-                    reject(new Error(`Unexpected status code for random.cat: ${res.statusCode}`));
-                } else {
-                    let kitty = JSON.parse(body).file;
-                    ctx.msg.channel.createMessage(kitty).then(resolve).catch(reject);
-                }
-            });
+            got('http://random.cat/meow').then(res => {
+                let nyaa = JSON.parse(res.body).file;
+                ctx.msg.channel.createMessage(nyaa).then(resolve).catch(reject);
+            }).catch(reject(new Error('Unexpected Error happened.')));
         });
     }
 };
