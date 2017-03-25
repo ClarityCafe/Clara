605e4e70c02940d9c679facc2e6c8543d7dede54
Ovyerus <iamovyerus@gmail.com>
2017-03-25 22:46:24 +1100

    Shuffle some things around in about

--- src/commands/about/about.js
+++ src/commands/about/about.js
@@ -1,18 +1,17 @@
-/* A brief info about the bot
+/* 
+ * about.js - A more indepth and specific information about the bot.
  * 
- * It's like info, but we tackle more about the copyright and such
- * 
- *  Contributed by Capuccino
+ * Contributed by Capuccino
  */
+
+const fs = require('fs');
 
 exports.commands = [
     'about'
 ];
 
-const fs = require('fs');
-
 exports.about = {
-    desc: 'All you need to know where the bot came from',
+    desc: 'All you need to know where the bot came from.',
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
             let version = JSON.parse(fs.readFileSync(`${__baseDir}/package.json`)).version;
@@ -21,7 +20,6 @@ exports.about = {
                 description: localeManager.t('about-nya', ctx.settings.locale, {github: 'https://github.com/awau/Clara', patreon: 'https://patreon.com/capuccino'}),
                 image: {url: 'https://github.com/awau/Clara/raw/master/nodebot_logo.png'},
                 footer: {text: `${bot.user.username} uses Clara ${version}`}
-
             }}).then(resolve).catch(reject);
         });
     }
