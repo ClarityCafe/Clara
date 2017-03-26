a5db9dacac6fa79744b930806a65cae4c5187fd3
sr229 <aguilarralph13@gmail.com>
2017-03-26 11:56:31 +0800

    fix localeManager not being found because a shit

--- src/bot.js
+++ src/bot.js
@@ -15,7 +15,7 @@ const CommandHolder = require(`${__dirname}/modules/CommandHolder`);
 global.__baseDir = __dirname;
 global.Promise = require('bluebird');
 global.logger = require(`${__dirname}/modules/logger`);
-global.localeManager = new (require(`${__dirname}/modules/LocaleManager`))();
+global.localeManager = new (require(`${__dirname}/modules/localeManager`))();
 
 // Setup stuff
 const config = require(`${__dirname}/config.json`);
