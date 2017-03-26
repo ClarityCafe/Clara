c77d8d7514cb5cc48df3fcf119706d0b27592715
Ovyerus <iamovyerus@gmail.com>
2017-03-26 17:27:51 +1100

    Fix about

--- src/commands/about/about.js
+++ src/commands/about/about.js
@@ -1,10 +1,18 @@
 /* 
  * about.js - A more indepth and specific information about the bot.
  * 
- * Contributed by Capuccino
+ * Contributed by Capuccino and Ovyerus
  */
 
 const fs = require('fs');
+const path = require('path');
+
+let version;
+try {
+    version = JSON.parse(fs.readFileSync(path.normalize(`${__baseDir}/../package.json`))).version;
+} catch(_) {
+    version = JSON.parse(fs.readFileSync(`${__baseDir}/package.json`)).version;
+}
 
 exports.commands = [
     'about'
@@ -14,7 +22,6 @@ exports.about = {
     desc: 'All you need to know where the bot came from.',
     main: (bot, ctx) => {
         return new Promise((resolve, reject) => {
-            let version = JSON.parse(fs.readFileSync(`${__baseDir}/package.json`)).version;
             ctx.msg.channel.createMessage({embed: {
                 title: 'About Clara',
                 description: localeManager.t('about-nya', ctx.settings.locale, {github: 'https://github.com/awau/Clara', patreon: 'https://patreon.com/capuccino'}),
