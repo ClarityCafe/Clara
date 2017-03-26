78143ad04cf8834be4eb959fbf8759657ccb9bc1
Ovyerus <iamovyerus@gmail.com>
2017-03-26 11:59:49 +1100

    Rewrite eval and add support for auto-editing for promises

--- src/commands/eval/eval.js
+++ src/commands/eval/eval.js
@@ -11,7 +11,11 @@ const utils = require(`${__baseDir}/modules/utils.js`);
 const fs = require('fs');
 const cp = require('child_process');
 const path = require('path');
+const got = require('got');
 /* eslint-enable */
+
+const FAILCOL = 0xF44336;
+const SUCCESSCOL = 0x8BC34A;
 
 exports.commands = [
     'eval'
@@ -27,41 +31,121 @@ exports.eval = {
             if (ctx.suffix.length === 0) {
                 ctx.msg.channel.createMessage('Please give arguments to evaluate.').then(resolve).catch(reject);
             } else {
-                let evalArgs = ctx.suffix;
                 let {msg, args, cmd, suffix, cleanSuffix, guildBot, settings} = ctx; // eslint-disable-line
                 try {
-                    let returned = eval(evalArgs);
-
-
+                    let returned = eval(ctx.suffix);
                     let str = util.inspect(returned, {depth: 1});
                     str = str.replace(new RegExp(bot.token, 'gi'), '(token)');
 
-                    let sentMessage = '```js\n';
-                    sentMessage += `Input: ${evalArgs}\n\n`;
-                    sentMessage += `Output: ${str}\n`;
-                    sentMessage += '```';
+                    let embed = {
+                        title: 'Evaluation Results',
+                        color: SUCCESSCOL,
+                        fields: [
+                            {name: 'Input', value: generateCodeblock(ctx.suffix)},
+                            {name: 'Output', value: generateCodeblock(str)}
+                        ]
+                    };
 
-                    if (sentMessage.length > 1897) {
-                        sentMessage = sentMessage.substr(0, 1897);
-                        sentMessage = sentMessage + '...\n````';
+                    if (returned instanceof Promise) embed.fields[1].value = generateCodeblock('[object Promise]');
+
+
+                    if (str.length < 1000) {
+                        sendEval(bot, ctx, embed, returned).then(resolve).catch(reject);
+                    } else {
+                        got('https://hastebin.com/documents', {
+                            method: 'POST',
+                            body: str
+                        }).then(res => {
+                            let key = JSON.parse(res.body).key;
+                            let url = `https://hastebin.com/${key}.js`;
+                            embed.fields[1].value = `Output is too long to display nicely.\nOutput has been uploaded [here](${url})`;
+
+                            return sendEval(bot, ctx, embed, returned);
+                        }).then(resolve).catch(reject);
                     }
-
-                    ctx.msg.channel.createMessage(sentMessage).then(resolve).catch(reject);
                 } catch(err) {
+                    let embed = {
+                        title: 'Evaluation Results',
+                        color: FAILCOL,
+                        fields: [
+                            {name: 'Input', value: generateCodeblock(ctx.suffix)},
+                            {name: 'Error', value: generateCodeblock(err)}
+                        ]
+                    };
 
-                    let errMessage = '```js\n';
-                    errMessage += `Input: ${evalArgs}\n\n`;
-                    errMessage += `${err}\n`;
-                    errMessage += '```';
-
-                    if (errMessage.length > 1897) {
-                        errMessage = errMessage.substr(0, 1897);
-                        errMessage = errMessage + '...\n```';
-                    }
-
-                    ctx.msg.channel.createMessage(errMessage).then(resolve).catch(reject);
+                    ctx.msg.channel.createMessage({embed}).then(resolve).catch(reject);
                 }
             }
         });
     }
 };
+
+function generateCodeblock(text) {
+    return `\`\`\`js\n${text}\n\`\`\``;
+}
+
+function sendEval(bot, ctx, embed, returned) {
+    return new Promise((resolve, reject) => {
+        let outer;
+        ctx.msg.channel.createMessage({embed}).then(m => {
+            outer = m;
+            if (returned instanceof Promise) {
+                return returned;
+            } else {
+                return null;
+            }
+        }).then(res => {
+            if (!res) {
+                resolve();
+                return null;
+            } else {
+                let strN = util.inspect(returned._rejectionHandler0, {depth: 1});
+                strN = strN.replace(new RegExp(bot.token, 'gi'), '(token)');
+
+                if (strN.length >= 1000) {
+                    return got('https://hastebin.com/documents', {
+                        method: 'POST',
+                        body: strN
+                    });
+                } else {
+                    return outer.edit({embed: {
+                        title: 'Evaluation Results',
+                        color: SUCCESSCOL,
+                        fields: [
+                            {name: 'Input', value: generateCodeblock(ctx.suffix)},
+                            {name: 'Output', value: generateCodeblock(strN)}
+                        ]
+                    }});
+                }
+            }
+        }).then(res => {
+            if (!res || res instanceof Eris.Message) return null;
+
+            let key = JSON.parse(res.body).key;
+            let url = `https://hastebin.com/${key}.js`;
+
+            return outer.edit({embed: {
+                title: 'Evaluation Results',
+                color: SUCCESSCOL,
+                fields: [
+                    {name: 'Input', value: generateCodeblock(ctx.suffix)},
+                    {name: 'Output', value: `Output is too long to display nicely.\nOutput has been uploaded [here](${url})`}
+                ]
+            }});
+        }).then(resolve).catch(err => {
+            if (err.req && err.req._headers.host === 'discordapp.com') {
+                reject(err);
+                return null;
+            } else {
+                return outer.edit({embed: {
+                    title: 'Evaluation Results',
+                    color: FAILCOL,
+                    fields: [
+                        {name: 'Input', value: generateCodeblock(ctx.suffix)},
+                        {name: 'Error', value: generateCodeblock(err)}
+                    ]
+                }});
+            }
+        });
+    });
+}
