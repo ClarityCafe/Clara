dadffc32426f485214f96c8fe8550261dab574ab
sr229 <aguilarralph13@gmail.com>
2017-03-30 18:52:35 +0800

    k fine fuck this revert

--- src/events/guilds.js
+++ src/events/guilds.js
@@ -12,7 +12,7 @@ module.exports = bot => {
     });
 
     bot.on('guildDelete', () => {
-        if (!(g.members.filter(m => m.bot).size/g.members.size >= 0.50)) {
+        if (!(g.members.filter(m => m.bot).size/g.members.size >= 0.75)) {
             bot.editStatus('online', {name: `${bot.config.gameName || `${bot.config.mainPrefix}help for commands!`} | ${bot.guilds.size} ${bot.guilds.size === 1 ? 'server' : 'servers'}`, type: bot.config.gameURL ? 1 : 0, url: `${bot.config.gameURL || null}`});
             bot.postGuildCount();
         }
