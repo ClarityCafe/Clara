9b1e0797711a439aea7b50ccc67ff9d09d947fc2
Capuccino <aguilarralph13@gmail.com>
2017-04-01 16:40:09 +0800

    fix saucenao?

--- src/commands/saucenao/saucenao.js
+++ src/commands/saucenao/saucenao.js
@@ -15,51 +15,53 @@ exports.saucenao = {
     longDesc: 'Gets the image source from sacuenao.',
     usage: '(Image URL [If not provided, it would grab the last message with image attachment\'s URL and sends it to the API])',
     main(bot, ctx) {
-        if (!ctx.suffix) {
-            //grab the last message with an attachment
-            if (!ctx.msg.channel.attachments) {
-                ctx.msg.channel.createMessage('Aw, no Image here.').then(reject);
-            } else {
-                let imgUrl = ctx.msg.channel.attachments[0].url;
-                got(`https://saucenao.com`, {
-                    method: 'POST',
-                    'Content-Type': 'application/json',
-                    body: JSON.stringify({url: imgUrl})
-                }).then(res => {
-                    //only return the top result
-                    const fields = [];
-                    fields.push({name: '', value: res[0].url, inline: true});
-                    //finally send the message in embed
-                    ctx.msg.channel.createMessage({
-                        embed: {
-                            title: 'saucenao Query',
-                            desccription: 'This is the closest I can find.',
-                            fields
-                        }
+        return new Promise((resolve, reject) => {
+            if (!ctx.suffix) {
+                //grab the last message with an attachment
+                if (!ctx.msg.channel.attachments) {
+                    ctx.msg.channel.createMessage('Aw, no Image here.').then(reject);
+                } else {
+                    let imgUrl = ctx.msg.channel.attachments[0].url;
+                    got(`https://saucenao.com`, {
+                        method: 'POST',
+                        'Content-Type': 'application/json',
+                        body: JSON.stringify({url: imgUrl})
+                    }).then(res => {
+                        //only return the top result
+                        const fields = [];
+                        fields.push({name: '', value: res[0].url, inline: true});
+                        //finally send the message in embed
+                        ctx.msg.channel.createMessage({
+                            embed: {
+                                title: 'saucenao Query',
+                                desccription: 'This is the closest I can find.',
+                                fields
+                            }
+                        }).then(resolve).catch(reject);
                     }).then(resolve).catch(reject);
-                }).then(resolve).catch(reject);
-            }
-        } else {
-            if (!imgRegex) { 
-                ctx.msg.channel.createMessage('Your URL is invalid!, Please make sure it\'s a valid image URL.');
+                }
             } else {
-                //query saucenao but this time, we use the suffix value
-                got('https://saucenao.com', {
-                    method: 'POST',
-                    'Content-Type': 'application/json',
-                    body: JSON.stringify({url: ctx.suffix})
-                }).then(res => {
-                    const fields = [];
-                    fields.push({name: '', value: res.url});
-                    ctx.msg.channel.createMessage({
-                        embed: {
-                            title: 'saucenao query',
-                            desccription: 'this is the closest I can find',
-                            fields
-                        }
-                    });
-                }).then(resolve).catch(reject);
+                if (!imgRegex) {
+                    ctx.msg.channel.createMessage('Your URL is invalid!, Please make sure it\'s a valid image URL.');
+                } else {
+                    //query saucenao but this time, we use the suffix value
+                    got('https://saucenao.com', {
+                        method: 'POST',
+                        'Content-Type': 'application/json',
+                        body: JSON.stringify({url: ctx.suffix})
+                    }).then(res => {
+                        const fields = [];
+                        fields.push({name: '', value: res.url});
+                        ctx.msg.channel.createMessage({
+                            embed: {
+                                title: 'saucenao query',
+                                desccription: 'this is the closest I can find',
+                                fields
+                            }
+                        });
+                    }).then(resolve).catch(reject);
+                }
             }
-        }
+        });
     }
 };
