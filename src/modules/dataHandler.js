a8aae6918a9766300742ed5c97a27e0112bf2bac
sr229 <aguilarralph13@gmail.com>
2017-04-12 14:38:23 +0800

    added datahandler
    with no things yet

---
+++ src/modules/dataHandler.js
@@ -0,0 +1,18 @@
+/**
+ *  Clara - Data Handler for Database related queries
+ * 
+ *  Contributed by Capuccino
+ *
+ */
+
+const rethonk = require('rethinkdbdash');
+const Eris = require('eris');
+
+class dataHandler {
+    static () {
+        /**
+         *  @todo probably make something to dynamically switch to each mode.
+         * 
+         */
+    }
+}
