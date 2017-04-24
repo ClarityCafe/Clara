7b6e742b4e1895c2048bdecd756ca96563d51de4
sr229 <chinodesuuu@gmail.com>
2017-04-24 11:34:31 +0800

    extend handler

--- src/commands/saucenao/sauceQueryHandler.js
+++ src/commands/saucenao/sauceQueryHandler.js
@@ -5,6 +5,7 @@
  */
 
 const got = require('got');
+const fs = require('fs');
 
 /** 
  * Query Handler for SauceNao-related requests.
@@ -16,15 +17,26 @@ class SauceHandler {
      * @param {Number} [options.dbMaskI] Mask for selecting specific indexes to ENABLE. dbmask=8191 will search all of the first 14 indexes. If intending to search all databases, the db=999 option is more appropriate.
      * @param {Number} [options.dbMask]  Mask for selecting specific indexes to DISABLE. dbmaski=8191 would search only indexes higher than the first 14. This is ideal when attempting to disable only certain indexes, while allowing future indexes to be included by default.
      * @param {Number} [options.outputType = 1] 0=normal html 1=xml api(not implemented) 2=json api.
+     * @param {Number} [options.numRes = 5] number to change maximum results requested. Default 5.
      * @param {String} apiKey  API Key for SauceNao.
      */
-    constructorP(options, apiKey) {
+    constructor(options, apiKey) {
         this.apiKey = apiKey;
         this.options = {
             outputType: 1,
             dbMask: this.dbMask,
-            dbMaskI: this.dbMaskI
+            dbMaskI: this.dbMaskI,
+            numRes: 5
         };
+    }
+    getSauce(file, link) {
+        if (file === typeof path) {
+            /** @todo figure out how to upload images/links in saucenao */
+            //do nothing for now
+        } else if (link) {
+            /** @todo figure out how to upload images/links in saucenao */
+            //do nothing for now
+        }
     }
 }
 
