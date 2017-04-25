0a7cecd7120577154a8b07085e71a23273191a87
sr229 <aguilarralph13@gmail.com>
2017-04-25 18:41:43 +0800

    redo handler

--- src/commands/saucenao/sauceQueryHandler.js
+++ src/commands/saucenao/sauceQueryHandler.js
@@ -6,18 +6,20 @@
 
 const got = require('got');
 const fs = require('fs');
+const urlRegex = str => /(http(s)?:\/\/)?(www\.)?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.test(str);
 
 /** 
  * Query Handler for SauceNao-related requests.
+ * PEPPYNOTE: Do not add Discord-Related hooks here as this would be backported to Sagiri
  */
 
 class SauceHandler {
     /**
      * @param {Object} options Query Options for queries.
-     * @param {Number} [options.dbMaskI] Mask for selecting specific indexes to ENABLE. dbmask=8191 will search all of the first 14 indexes. If intending to search all databases, the db=999 option is more appropriate.
-     * @param {Number} [options.dbMask]  Mask for selecting specific indexes to DISABLE. dbmaski=8191 would search only indexes higher than the first 14. This is ideal when attempting to disable only certain indexes, while allowing future indexes to be included by default.
-     * @param {Number} [options.outputType = 2] 0=normal html 1=xml api(not implemented) 2=json api.
-     * @param {Number} [options.numRes = 5] number to change maximum results requested. Default 5.
+     * @param {Number} options.dbMaskI Mask for selecting specific indexes to ENABLE. dbmask=8191 will search all of the first 14 indexes. If intending to search all databases, the db=999 option is more appropriate.
+     * @param {Number} options.dbMask  Mask for selecting specific indexes to DISABLE. dbmaski=8191 would search only indexes higher than the first 14. This is ideal when attempting to disable only certain indexes, while allowing future indexes to be included by default.
+     * @param {Number} options.outputType 0=normal html 1=xml api(not implemented) 2=json api.
+     * @param {Number} options.numRes number to change maximum results requested. Default 5.
      * @param {String} apiKey  API Key for SauceNao.
      */
     constructor(options, apiKey) {
@@ -34,6 +36,11 @@ class SauceHandler {
             /** @todo figure out how to upload images/links in saucenao */
             //do nothing for now
         } else if (link) {
+            if (!urlRegex === link) {
+                throw new Error('Link is not valid HTTP/HTTPS Address.');
+            } else {
+                got('').then();
+            }
             /** @todo figure out how to upload images/links in saucenao */
             //do nothing for now
         }
