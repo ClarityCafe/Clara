428b70d8f638f6da7701f093909365c68fa8f5c3
Capuccino <chinodesuuu@gmail.com>
2017-05-08 11:14:31 +0800

    rewrote saucenao

--- src/commands/saucenao/saucenao.js
+++ src/commands/saucenao/saucenao.js
@@ -7,10 +7,37 @@
 
 /* eslint-env node */
 
+//this handles the SauceNao handling
+const ayaneru = new (require('./sauceQueryHandler'))();
+//this regex is gay
+
+const urlRegex = str => /(http(s)?:\/\/)?(www\.)?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.test(str);
+
 exports.commands = [
     'saucenao'
 ];
 
 exports.saucenao = {
-
+    desc: 'Gets the image from the recent attachment or via a image link and looks for saucenao to check for the source of the image.',
+    main(bot, ctx) { 
+        return new Promise((resolve, reject) => {
+            if (!ctx.attachments[0]) {
+                return ctx.createMessage('Aw, no image here.');
+            } else if (!ctx.suffix === urlRegex) {
+                return ctx.createMessage('Oi, your URL is invalid!');
+            } else if (ctx.attachments[0]) {
+                ayaneru.getSauce(ctx.attachments[0].url).then(res => {
+                    for (res.url of res) {
+                        var fields = [];
+                        fields.push(`${{name: 'Item', value: res.url, inline: true}}`);
+                    }
+                    ctx.createMessage({embed: {
+                        title: 'Saucenao query results',
+                        description: 'This is the closest I can find. Sorry if it is not accurate.',
+                        fields
+                    }}).then(resolve).catch(reject);
+                });
+            }
+        });
+    }
 };
