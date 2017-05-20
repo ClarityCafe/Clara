b8922ae4a2bca5c6691fc9ee4705fdd4031146f0
Ovyerus <iamovyerus@gmail.com>
2017-05-20 11:20:32 +1000

    Remove obsolete comment block

--- src/events/voice.js
+++ src/events/voice.js
@@ -2,16 +2,6 @@
 
 module.exports = bot => {
     bot.on('voiceChannelLeave', (mem, chan) => {
-        /*if (chan.voiceMembers.filter(m => m.id !== bot.user.id && !m.bot).length === 0 && chan.voiceMembers.get(bot.user.id)) {
-            setTimeout(() => {
-                let c = bot.guilds.get(chan.guild.id).channels.get(chan.id);
-                if (c.voiceMembers.filter(m => m.id !== bot.user.id && !m.bot).length === 0) {
-                    if (bot.music.connections.get(chan.guild.id).playing) bot.music.connetions.get(chan.guild.id).stopPlaying();
-                    bot.music.connections.get(chan.guild.id).leave();
-                }
-            }, 60000);
-        }*/
-
         if (mem.id !== bot.user.id && chan.voiceMembers.get(bot.user.id) && chan.voiceMembers.filter(m => m.id !== bot.user.id && !m.bot).length === 0) {
             setTimeout(() => {
                 if (chan.voiceMembers.filter(m => m.id !== bot.user.id && !m.bot).length === 0) {
