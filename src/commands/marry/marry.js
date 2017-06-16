d6459fbd6b170462a06074743e378fe77bd0b83a
sr229 <aguilarralph13@gmail.com>
2017-06-16 13:37:18 +0800

    bad joke xd

--- src/commands/marry/marry.js
+++ src/commands/marry/marry.js
@@ -22,6 +22,8 @@ exports.marry = {
                 ctx.createMessage("Hey! You're not allowed to marry yourself.").then(resolve).catch(reject);
             } else if (ctx.mentionStrings.length > 1) {
                 ctx.createMessage('One wife only!');
+            } else if (ctx.mentionStrings[0] === ctx.bot.id) {
+                ctx.createMessage("I-It's not like I'll say yes or anything >.<");
             } else {
                 ctx.createMessage(`<@${ctx.mentionStrings[0]}> will you marry ${ctx.author.mention}?\nRespond with yes or no. (30 seconds)`).then(() => {
                     return bot.awaitMessage(ctx.channel.id, ctx.mentionStrings[0], () => true, 30000);
