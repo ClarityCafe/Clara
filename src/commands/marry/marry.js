aaf22b7a1a5247d9d139e73dc910766b6adccf49
sr229 <aguilarralph13@gmail.com>
2017-07-06 20:49:50 +0800

    convert Marry to Locale Engine

--- src/commands/marry/marry.js
+++ src/commands/marry/marry.js
@@ -17,27 +17,27 @@ exports.marry = {
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
             if (ctx.args.length === 0 || ctx.mentionStrings.length === 0) {
-                ctx.createMessage('Please mention your partner.').then(resolve).catch(reject);
+                ctx.createMessage(localeManager.t('marry-noPartner', ctx.settings.locale)).then(resolve).catch(reject);
             } else if (ctx.mentionStrings[0] === ctx.author.id) {
-                ctx.createMessage("Hey! You're not allowed to marry yourself.").then(resolve).catch(reject);
+                ctx.createMessage(localeManager.t('marry-noSelfCest', ctx.settings.locale)).then(resolve).catch(reject);
             } else if (ctx.mentionStrings.length > 1) {
-                ctx.createMessage('One wife only!');
+                ctx.createMessage(localeManager.t('marry-tooManyMentions', ctx.settings.locale));
             } else if (ctx.mentionStrings[0] === bot.user.id) {
-                ctx.createMessage("I-It's not like I'll say yes or anything >.<");
+                ctx.createMessage(localeManager.t('marry-botMentioned', ctx.settings.locale));
             } else {
-                ctx.createMessage(`<@${ctx.mentionStrings[0]}> will you marry ${ctx.author.mention}?\nRespond with yes or no. (30 seconds)`).then(() => {
+                ctx.createMessage(localeManager.t('marry-PartnerPrompt', ctx.settings.locale, {author: ctx.author.mention, mentioned: `<@${ctx.mentionStrings[0]}>`})).then(() => {
                     return bot.awaitMessage(ctx.channel.id, ctx.mentionStrings[0], () => true, 30000);
                 }).then(m => {
                     if (/y(es)?/i.test(m.content)) {
-                        return ctx.createMessage(`I hereby pronounce <@${ctx.mentionStrings[0]}> and ${ctx.author.mention} as husband and wife. :sparkling_heart:`);
+                        return ctx.createMessage(localeManager.t('marry-successfulMarriage', ctx.settings.locale, {author: ctx.author.mention, mentioned: `<@${ctx.mentionStrings[0]}>`}));
                     } else if (/no?/i.test(m.content)) {
-                        return ctx.createMessage(`${ctx.author.mention}, unfortunately, your partner declined.`);
+                        return ctx.createMessage(localeManager.t('marry-marriageDeclined', ctx.settings.locale, {author: ctx.author.mention}));
                     } else {
-                        return ctx.createMessage(`<@${ctx.mentionStrings[0]}> that is not a valid answer.`);
+                        return ctx.createMessage(localeManager.t('marry-invalidAnswer', ctx.settings.locale, {mentioned: `<@${ctx.mentionStrings[0]}>`}));
                     }
                 }).then(resolve).catch(err => {
                     if (err.message && err.message === 'Message await expired.') {
-                        return ctx.createMessage(`${ctx.author.mention} your partner did not respond in time.`);
+                        return ctx.createMessage(localeManager.t('marry-timeout', ctx.settings.locale, {author: ctx.author.mention}));
                     } else {
                         reject(err);
                     }
