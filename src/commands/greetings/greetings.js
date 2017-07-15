e0fd9c25e1f6c051c9198d9ebe6e79bf98863f12
Ovyerus <iamovyerus@gmail.com>
2017-07-15 22:01:13 +1000

    Make greetings use subcommands

--- src/commands/greetings/greetings.js
+++ src/commands/greetings/greetings.js
@@ -5,95 +5,102 @@
 
 /* eslint-env node */
 
-const Eris = require('eris');
+exports.loadAsSubcommands = true;
 
 exports.commands = [
-    'greetings'
+    'enable',
+    'disable',
+    'channel',
+    'text'
 ];
 
-exports.greetings = {
+exports.main = {
     desc: 'Manage server greetings.',
-    longDesc: 'Show current greeting settings for the server, and manage them if you have the correct permission.',
+    permissions: {author: 'manageGuild'},
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
-            if (ctx.args.length === 0) {
-                ctx.createMessage({embed: greetingBlock(ctx.settings)}).then(resolve).catch(reject);
-            } else if (ctx.args[0] === 'enable') {
-                if (!ctx.member.permission.has('manageGuild')) {
-                    ctx.createMessage('user-noPerm', null, 'channel', {perm: 'Manage Server'}).then(resolve).catch(reject);
-                } else {
-                    bot.getGuildSettings(ctx.guild.id).then(res => {
-                        let settings = res;
-                        settings.greeting.enabled = true;
-                        return bot.setGuildSettings(res.id, settings);
-                    }).then(() => {
-                        return ctx.createMessage('greetings-enable');
-                    }).then(resolve).catch(reject);
-                }
-            }  else if (ctx.args[0] === 'disable') {
-                if (!ctx.member.permission.has('manageGuild')) {
-                    ctx.createMessage('user-noPerm', null, 'channel', {perm: 'Manage Server'}).then(resolve).catch(reject);
-                } else {
-                    bot.getGuildSettings(ctx.guild.id).then(res => {
-                        let settings = res;
-                        settings.greeting.enabled = false;
-                        return bot.setGuildSettings(res.id, settings);
-                    }).then(() => {
-                        return ctx.createMessage('greetings-disable');
-                    }).then(resolve).catch(reject);
-                }
-            } else if (ctx.args[0] === 'channel' && ctx.args.length >= 2) {
-                if (!ctx.member.permission.has('manageGuild')) {
-                    ctx.createMessage('user-noPerm', null, 'channel', {perm: 'Manage Server'}).then(resolve).catch(reject);
-                } else {
-                    bot.getGuildSettings(ctx.guild.id).then(res => {
-                        let settings = res;
+            ctx.createMessage({embed: greetingBlock(ctx.settings)}).then(resolve).catch(reject);
+        });
+    }
+};
 
-                        if (ctx.channelMentions.length > 0) {
-                            settings.greeting.channelID = ctx.channelMentions[0];
-                            return bot.setGuildSettings(res.id, settings);
-                        } else {
-                            let meme = ctx.suffix.split(' ');
-                            meme.shift();
-                            let chans = ctx.guild.channels.filter(c => c.name.toLowerCase().includes(meme.join(' ')));
+exports.enable = {
+    desc: 'Enable greetings.',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            ctx.settings.guild.greeting.enabled = true;
 
-                            if (chans.length === 0) {
-                                return ctx.createMessage('greetings-noChan', null, 'channel', {name: meme.join(' ')});
-                            } else {
-                                settings.greeting.channelID = chans[0].id;
-                                return bot.setGuildSettings(res.id, settings);
-                            }
-                        }
-                    }).then(res => {
-                        if (res instanceof Eris.Message) {
-                            return null;
-                        } else {
-                            return bot.getGuildSettings(ctx.guild.id);
-                        }
-                    }).then(res => {
-                        if (!res) {
-                            return null;
-                        } else {
-                            console.log(res);
-                            return ctx.createMessage('greetings-setChan', null, 'channel', {id: res.greeting.channelID});
-                        }
-                    }).then(resolve).catch(reject);
-                }
-            } else if (ctx.args[0] === 'text' && ctx.args.length >= 2) {
-                bot.getGuildSettings(ctx.guild.id).then(res => {
-                    let settings = res;
-                    let message = ctx.suffix.split(' ');
-                    message.shift();
-                    message = message.join(' ');
+            bot.setGuildSettings(ctx.guild.id, ctx.settings.guild).then(() => {
+                return ctx.createMessage('greetings-enable');
+            }).then(resolve).catch(reject);
+        });
+    }
+};
 
-                    settings.greeting.message = message;
+exports.disable = {
+    desc: 'Disable greetings.',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            ctx.settings.guild.greeting.enabled = false;
 
-                    return bot.setGuildSettings(res.id, settings);
-                }).then(() => {
-                    return ctx.createMessage('greetings-setMsg');
-                }).then(resolve).catch(reject);
+            return bot.setGuildSettings(ctx.guild.id, ctx.settings.guild).then(() => {
+                return ctx.createMessage('greetings-disable');
+            }).then(resolve).catch(reject);
+        });
+    }
+};
+
+exports.channel = {
+    desc: 'Set the channel for greetings.',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            if (!ctx.suffix) {
+                ctx.createMessage('greetings-noChanArgs').then(resolve).catch(reject);
             } else {
-                ctx.createMessage({embed: greetingBlock(ctx.settings)}).then(resolve).catch(reject);
+                if (ctx.channelMentions.length > 0) {
+                    ctx.settings.guild.greeting.channelID = ctx.channelMentions[0];
+
+                    bot.setGuildSettings(ctx.guild.id, ctx.settings.guild).then(() => {
+                        return ctx.createMessage('greetings-setChan', null, 'channel', {
+                            id: ctx.settings.guild.greeting.channelID
+                        });
+                    }).then(resolve).catch(reject);
+                } else {
+                    let chans = ctx.guild.channels.filter(c => c.name.toLowerCase().includes(ctx.suffix.toLowerCase()));
+
+                    if (chans.length === 0) {
+                        ctx.createMessage('greetings-noChan', null, 'channel', {
+                            name: ctx.suffix
+                        }).then(resolve).catch(reject);
+                    } else {
+                        ctx.settings.guild.greeting.channelID = chans[0].id;
+
+                        bot.setGuildSettings(ctx.guild.id, ctx.settings.guild).then(() => {
+                            return ctx.createMessage('greetings-setChan', null, 'channel', {
+                                id: ctx.settings.guild.greeting.channelID
+                            });
+                        }).then(resolve).catch(reject);
+                    }
+                }
+            }
+        });
+    }
+};
+
+exports.text = {
+    desc: 'Set the greetings text.',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            if (!ctx.suffix) {
+                ctx.createMessage('greetings-noMsgArgs').then(resolve).catch(reject);
+            } else {
+                ctx.settings.guild.greeting.message = ctx.suffix;
+
+                bot.setGuildSettings(ctx.guild.id, ctx.settings.guild).then(() => {
+                    return ctx.createMessage('greetings-setMsg');
+                }).then(() => {
+                    return ctx.createMessage(ctx.suffix.replace(/{{user}}/gi, ctx.author.mention).replace(/{{name}}/gi, ctx.author.username));
+                }).then(resolve).catch(reject);
             }
         });
     }
