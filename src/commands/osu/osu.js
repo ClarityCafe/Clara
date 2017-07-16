0bfc67e641f476610cc9eeff58b7cfbb4ce6ecc8
Ovyerus <iamovyerus@gmail.com>
2017-07-16 19:56:08 +1000

    Ratelimit osu

--- src/commands/osu/osu.js
+++ src/commands/osu/osu.js
@@ -1,28 +1,34 @@
-/*
- * osu.js - osu! stats command.
- * 
- * Contributed by Capuccino and Ovyerus.
+/**
+ * @file osu! stats command.
+ * @author Capuccino
+ * @author Ovyerus
  */
 
 /* eslint-env node */
 
-const config = require(`${__baseDir}/config.json`);
-const osu = require('osu')(config.osuApiKey);
+const Osu = require('osu');
 const iso3166 = require('iso-3166-1-alpha-2');
+const Ratelimiter = require(`${__baseDir}/modules/Ratelimiter`);
 
-const commaRegex = /\B(?=(\d{3})+(?!\d))/g; // Thank you Brayzure uwu
+const CommaRegex = /\B(?=(\d{3})+(?!\d))/g; // Thank you Brayzure uwu
+const B1nzy = new Ratelimiter(240, 60000); // 240 uses per minute
+
+let osu;
 
 exports.loadAsSubcommands = true;
 
 exports.commands = [
     'ctb',
-    'maina',
+    'mania',
     'taiko'
 ];
 
+exports.init = bot => {
+    osu = Osu(bot.config.osuApiKey);
+};
+
 exports.main = {
     desc: "Retrive stats for osu!'s standard mode for a user.",
-    fullDesc: "Uses the osu! API to get information for osu!'s standard mode for a user.",
     usage: '<osu! username/id>',
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
@@ -30,60 +36,22 @@ exports.main = {
                 ctx.createMessage(localeManager.t('osu-noName', ctx.settings.locale)).then(resolve).catch(reject);
             } else {
                 ctx.channel.sendTyping();
-                osu.get_user({u: ctx.suffix}, res => {
-                    var user = res[0];
-                    if (!user || !user.user_id) {
-                        ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
-                    } else {
-                        ctx.createMessage(osuBlock(user, ctx.settings)).then(resolve).catch(reject);
-                    }
-                });
-            }
-        });
-    }
-};
 
-exports.ctb = {
-    desc: "Retrive stats for osu!'s Catch The Beat mode for a user.",
-    fullDesc: "Uses the osu! API to get information for osu!'s Catch The Beat mode for a user.",
-    usage: '<osu! username/id>',
-    main(bot, ctx) {
-        return new Promise((resolve, reject) => {
-            if (ctx.suffix.length === 0) {
-                ctx.createMessage(localeManager.t('osu-noName', ctx.settings.locale)).then(resolve).catch(reject);
-            } else {
-                ctx.channel.sendTyping();
-                osu.get_user({u: ctx.suffix, m: 2}, res => {
-                    var user = res[0];
-                    if (!user || !user.user_id) {
-                        ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
-                    } else {
-                        ctx.createMessage(osuBlock(user, ctx.settings)).then(resolve).catch(reject);
-                    }
-                });
-            }
-        });
-    }
-};
+                if (B1nzy.uses === B1nzy.totalUses) {
+                    ctx.createMessage('osu-ratelimited').then(resolve).catch(reject);
+                } else {
+                    osu.get_user({u: ctx.suffix}, res => {
+                        let user = res[0];
 
-exports.mania = {
-    desc: "Retrive stats for osu!'s osu!mania mode for a user.",
-    fullDesc: "Uses the osu! API to get information for osu!'s osu!mania mode for a user.",
-    usage: '<osu! username/id>',
-    main(bot, ctx) {
-        return new Promise((resolve, reject) => {
-            if (ctx.suffix.length === 0) {
-                ctx.createMessage(localeManager.t('osu-noName', ctx.settings.locale)).then(resolve).catch(reject);
-            } else {
-                ctx.channel.sendTyping();
-                osu.get_user({u: ctx.suffix, m: 3}, res => {
-                    var user = res[0];
-                    if (!user || !user.user_id) {
-                        ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
-                    } else {
-                        ctx.createMessage(osuBlock(user, ctx.settings)).then(resolve).catch(reject);
-                    }
-                });
+                        if (!user || !user.user_id) {
+                            ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
+                        } else {
+                            ctx.createMessage(osuBlock(user, ctx.settings)).then(() => {
+                                B1nzy.use();
+                            }).then(resolve).catch(reject);
+                        }
+                    });
+                }
             }
         });
     }
@@ -91,7 +59,6 @@ exports.mania = {
 
 exports.taiko = {
     desc: "Retrive stats for osu!'s Taiko mode for a user.",
-    fullDesc: "Uses the osu! API to get information for osu!'s Taiko mode for a user.",
     usage: '<osu! username/id>',
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
@@ -99,14 +66,82 @@ exports.taiko = {
                 ctx.createMessage(localeManager.t('osu-noName', ctx.settings.locale)).then(resolve).catch(reject);
             } else {
                 ctx.channel.sendTyping();
-                osu.get_user({u: ctx.suffix, m: 1}, res => {
-                    var user = res[0];
-                    if (!user || !user.user_id) {
-                        ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
-                    } else {
-                        ctx.createMessage(osuBlock(user, ctx.settings)).then(resolve).catch(reject);
-                    }
-                });
+
+                if (B1nzy.uses === B1nzy.totalUses) {
+                    ctx.createMessage('osu-ratelimited').then(resolve).catch(reject);
+                } else {                
+                    osu.get_user({u: ctx.suffix, m: 3}, res => {
+                        let user = res[0];
+
+                        if (!user || !user.user_id) {
+                            ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
+                        } else {
+                            ctx.createMessage(osuBlock(user, ctx.settings)).then(() => {
+                                B1nzy.use();
+                            }).then(resolve).catch(reject);
+                        }
+                    });
+                }
+            }
+        });
+    }
+};
+
+exports.ctb = {
+    desc: "Retrive stats for osu!'s Catch The Beat mode for a user.",
+    usage: '<osu! username/id>',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            if (ctx.suffix.length === 0) {
+                ctx.createMessage(localeManager.t('osu-noName', ctx.settings.locale)).then(resolve).catch(reject);
+            } else {
+                ctx.channel.sendTyping();
+
+                if (B1nzy.uses === B1nzy.totalUses) {
+                    ctx.createMessage('osu-ratelimited').then(resolve).catch(reject);
+                } else {                
+                    osu.get_user({u: ctx.suffix, m: 2}, res => {
+                        let user = res[0];
+
+                        if (!user || !user.user_id) {
+                            ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
+                        } else {
+                            ctx.createMessage(osuBlock(user, ctx.settings)).then(() => {
+                                B1nzy.use();
+                            }).then(resolve).catch(reject);
+                        }
+                    });
+                }
+            }
+        });
+    }
+};
+
+exports.mania = {
+    desc: "Retrive stats for osu!'s osu!mania mode for a user.",
+    usage: '<osu! username/id>',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            if (ctx.suffix.length === 0) {
+                ctx.createMessage(localeManager.t('osu-noName', ctx.settings.locale)).then(resolve).catch(reject);
+            } else {
+                ctx.channel.sendTyping();
+
+                if (B1nzy.uses === B1nzy.totalUses) {
+                    ctx.createMessage('osu-ratelimited').then(resolve).catch(reject);
+                } else {                
+                    osu.get_user({u: ctx.suffix, m: 3}, res => {
+                        let user = res[0];
+
+                        if (!user || !user.user_id) {
+                            ctx.createMessage(localeManager.t('osu-noRes', ctx.settings.locale)).then(resolve).catch(reject);
+                        } else {
+                            ctx.createMessage(osuBlock(user, ctx.settings)).then(() => {
+                                B1nzy.use();
+                            }).then(resolve).catch(reject);
+                        }
+                    });
+                }
             }
         });
     }
@@ -121,20 +156,20 @@ function osuBlock(user, settings) {
         fields: [
             {name: localeManager.t('id', settings.locale), value: user.user_id, inline: true},
             {name: localeManager.t('osu-country', settings.locale), value: `${iso3166.getCountry(user.country)} :flag_${user.country.toLowerCase()}:`, inline: true},
-            {name: localeManager.t('osu-countryRank', settings.locale), value: user.pp_country_rank.replace(commaRegex, ','), inline: true},
-            {name: localeManager.t('osu-playCount', settings.locale), value: user.playcount ? user.playcount.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-300s', settings.locale), value: user.count300 ? user.count300.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-100s', settings.locale), value: user.count100 ? user.count100.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-50s', settings.locale), value: user.count50 ? user.count50.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-rankedScore', settings.locale), value: user.ranked_score ? user.ranked_score.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-totalScore', settings.locale), value: user.total_score ? user.total_score.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-pp', settings.locale), value: user.pp_raw ? user.pp_raw.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-level', settings.locale), value: user.level ? Number(user.level).toFixed(1).toString().replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-rank', settings.locale), value: user.pp_rank ? user.pp_rank.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-countryRank', settings.locale), value: user.pp_country_rank.replace(CommaRegex, ','), inline: true},
+            {name: localeManager.t('osu-playCount', settings.locale), value: user.playcount ? user.playcount.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-300s', settings.locale), value: user.count300 ? user.count300.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-100s', settings.locale), value: user.count100 ? user.count100.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-50s', settings.locale), value: user.count50 ? user.count50.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-rankedScore', settings.locale), value: user.ranked_score ? user.ranked_score.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-totalScore', settings.locale), value: user.total_score ? user.total_score.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-pp', settings.locale), value: user.pp_raw ? user.pp_raw.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-level', settings.locale), value: user.level ? Number(user.level).toFixed(1).toString().replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-rank', settings.locale), value: user.pp_rank ? user.pp_rank.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
             {name: localeManager.t('osu-accuracy', settings.locale), value: user.accuracy ? Number(user.accuracy).toFixed(2).toString('utf8') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-ss', settings.locale), value: user.count_rank_ss ? user.count_rank_ss.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-s', settings.locale), value: user.count_rank_s ? user.count_rank_s.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
-            {name: localeManager.t('osu-a', settings.locale), value: user.count_rank_a ? user.count_rank_a.replace(commaRegex, ',') : localeManager.t('none', settings.locale), inline: true}
+            {name: localeManager.t('osu-ss', settings.locale), value: user.count_rank_ss ? user.count_rank_ss.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-s', settings.locale), value: user.count_rank_s ? user.count_rank_s.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true},
+            {name: localeManager.t('osu-a', settings.locale), value: user.count_rank_a ? user.count_rank_a.replace(CommaRegex, ',') : localeManager.t('none', settings.locale), inline: true}
         ]
     }};
 }
