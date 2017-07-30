33c1209ef9636659c0f82e28f693632a5fc18d01
Ovyerus <iamovyerus@gmail.com>
2017-07-30 17:01:50 +1000

    Formatting

--- src/commands/leetspeak/leetspeak.js
+++ src/commands/leetspeak/leetspeak.js
@@ -2,21 +2,24 @@
  * @file leetspeak generator nya
  * @author Capuccino
  */
- exports.commands = [
-     'leet'
- ];
 
- const leet = require('l33tsp34k');
- exports.leet = {
-     desc: 'L33tify your message',
-     usage: '<message>',
-     main(bot, ctx) {
-         return new Promise((resolve, reject) => {
-             if (!ctx.suffix) {
-                 return ctx.createMessage('Provide me a Message to l33tify').then(resolve).catch(reject);
-             } else {
-                 return ctx.createMessage(leet(ctx.suffix));
-             }
-         });
-     }
- };
+const leet = require('l33tsp34k');
+
+exports.commands = [
+    'leet'
+];
+
+
+exports.leet = {
+    desc: 'L33tify your message',
+    usage: '<message>',
+    main(bot, ctx) {
+        return new Promise((resolve, reject) => {
+            if (!ctx.suffix) {
+                return ctx.createMessage('Provide me a message to l33tify').then(resolve).catch(reject);
+            } else {
+                return ctx.createMessage(leet(ctx.suffix));
+            }
+        });
+    }
+};
