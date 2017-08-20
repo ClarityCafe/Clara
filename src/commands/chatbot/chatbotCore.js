d91588421c1ee6b86bbb2a69c4228c42e87bb249
sr229 <aguilarralph13@gmail.com>
2017-08-20 12:43:24 +0800

    add more params

--- src/commands/chatbot/chatbotCore.js
+++ src/commands/chatbot/chatbotCore.js
@@ -10,20 +10,31 @@
   */
  class Chatbot {
      /**
-      * 
       * @param {String} dataPath path of where the data is
       * @param {String} convoFile the corpus file for convos
       * @param {String} lineFile the corpus file for lines
       * @param {String} outputFile  the output corpus file 
       * @param {String} processedPath the path on where to dump processed files for caching
       * @param {String} checkpoints path of where to place the checkpoints
+      * @param {Number} treshold the treshold for collision
+      * @param {Number} padID *
+      * @param {Number} unkID *
+      * @param {Number} startID * 
+      * @param {Number} eosID *
+      * @param {Number} testSetSize the size of the test sets
       */
-     constructor(dataPath, convoFile, lineFile, outputFile, processedPath, checkpoints) {
+     constructor(dataPath, convoFile, lineFile, outputFile, processedPath, checkpoints, treshold, padID, unkID, startID, eosID, testSetSize) {
          this.dataPath = dataPath,
          this.convoFile = convoFile,
          this.outputFile = outputFile,
          this.processedPath = processedPath,
-         this.checkpoints =checkpoints;
+         this.checkpoints = checkpoints,
+         this.treshold = treshold,
+         this.padID = padID,
+         this.unkID = unkID,
+         this.startID = startID,
+         this.eosID = eosID,
+         this.testSetSize = testSetSize || 25000;
      }
  }
 
