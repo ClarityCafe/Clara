e93fa4c70de50ad12b5730414224b58ad8081c55
Capuccino <chinodesuuu@gmail.com>
2017-09-15 09:45:08 +0800

    introduce the proposed command example

---
+++ example/0.4.x/example.js
@@ -0,0 +1,37 @@
+/*
+ * example.js - Example command to show basic structure of commands.
+ * 
+ * Contributed by Capuccino, Ovyerus.
+ */
+
+
+
+exports.commands = [
+    // Define the names of your commands here.
+    'ping',
+    'pong'
+];
+
+// Example ping command.
+exports.ping = {
+    desc: '',
+    fullDesc: '',
+    adminOnly: true,
+    usage: '',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            ctx.createMessage('Ping!').then(resolve).catch(reject);
+        });
+    }
+};
+
+// Example pong command (demonstrating a full command, and multiple commands in a file).
+exports.pong = {
+    desc: 'Ping!',
+    fullDesc: 'Simple response command.',
+    main: (bot, ctx) => {
+        return new Promise((resolve, reject) => {
+            ctx.createMessage('Pong!').then(resolve).catch(reject);
+        });
+    }
+};
