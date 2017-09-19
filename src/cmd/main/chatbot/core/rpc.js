9ef0e458620fc117ddda30ab713f86e04e3d19dd
sr229 <aguilarralph13@gmail.com>
2017-09-19 21:08:07 +0800

    how to PEP8 and check stringlength plo0x

--- src/cmd/main/chatbot/core/rpc.js
+++ src/cmd/main/chatbot/core/rpc.js
@@ -6,24 +6,26 @@
 /**
  * @see {Link} https://stackoverflow.com/questions/10775351/combining-node-js-and-python 
  */
- const zeropc = require('zeropc');
- const rpc = new zeropc.Client();
- const res = new zeropc.Server({
-     // TODO: make it return JSON from the Pythonk server 
- })
+const zerorpc = require('zerorpc');
+const rpc = new zerorpc.Client();
+const res = new zerorpc.Server({
+    // TODO: make it return JSON from the Pythonk server 
+});
  
- // make sure we bind this to localhost, probably use a config file for chatbot RPC
- rpc.connect('http://localhost:3000');
- // binding for server return
- res.bind('http://localhost:4000');
+// make sure we bind this to localhost, probably use a config file for chatbot RPC
+rpc.connect('http://localhost:3000');
+// binding for server return
+res.bind('http://localhost:4000');
  
- /**
+/**
   * @todo add function for reply and send (event based)
   */
  
- rpc.invoke('outgoing', (error, reply) => {
-     if (!err) {
-         return 
-     }
- })
+rpc.invoke('incoming', (error, reply) => {
+    if (!error) {
+          
+    } else {
+        return error;
+    }
+});
  
