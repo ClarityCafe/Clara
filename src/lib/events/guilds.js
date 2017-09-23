5f1c05681c130e8970b95a57b17b7b3741d932aa
Ovyerus <Ovyerus@users.noreply.github.com>
2017-09-23 17:01:01 +1000

    Revert thing that doesn't work

--- src/lib/events/guilds.js
+++ src/lib/events/guilds.js
@@ -10,7 +10,7 @@ module.exports = bot => {
     bot.on('guildCreate', g => {
         if (g.members.filter(m => m.bot).size / g.members.size >= 0.50) {
             logger.info(`Leaving bot collection '${g.name}' (${g.id})`);
-            g.leaveGuild(g.id);
+            g.leave();
         } else {
             bot.editStatus('online', {name: `${bot.config.gameName || `${bot.config.mainPrefix}help for commands!`} | ${bot.guilds.size} ${bot.guilds.size === 1 ? 'server' : 'servers'}`, type: bot.config.gameURL ? 1 : 0, url: `${bot.config.gameURL || null}`});
             bot.postGuildCount();
