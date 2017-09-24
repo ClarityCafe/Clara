9d559cf44dad8c80f0fb45595279c752582c09d9
sr229 <aguilarralph13@gmail.com>
2017-09-24 16:17:00 +0800

    fix choose not working as intended

--- src/commands/choose/choosemedesu.js
+++ src/commands/choose/choosemedesu.js
@@ -12,17 +12,16 @@ exports.commands = [
 
 exports.choose = {
     desc: 'Randomly chooses between 2 or more arguments.',
-    fullDesc: 'Uses a randomiser to pick a random value out of two given arguments.',
-    usage: '<choice1> <choice2>',
-    example: '"coke zero" coke',
+    usage: '<choice 1>/<choice 2>/...',
+    example: 'coke zero/coke',
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
             let choices = ctx.suffix.split('/');
             if (choices.length < 2) {
-                ctx.msg.channel.createMessage(localeManager.t('choose-insufficientArgs', ctx.settings.locale)).then(resolve).catch(reject);
+                ctx.createMessage('choose-insufficientArgs').then(resolve).catch(reject);
             } else {
                 var choice = choices[Math.floor(Math.random() * choices.length)];
-                ctx.msg.channel.createMessage(localeManager.t('choose', ctx.settings.locale, {name: ctx.msg.author.username, choice})).then(resolve).catch(reject);
+                ctx.createMessage(localeManager.t('choose', ctx.settings.locale, {name: ctx.msg.author.username, choice})).then(resolve).catch(reject);
             }
         });
     }
