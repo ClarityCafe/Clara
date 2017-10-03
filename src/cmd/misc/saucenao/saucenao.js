bac2a39ea7142588ca8ac135605cba68f90a9a61
Ovyerus <Ovyerus@users.noreply.github.com>
2017-10-03 19:35:51 +1100

    Update saucenao command

--- src/cmd/misc/saucenao/saucenao.js
+++ src/cmd/misc/saucenao/saucenao.js
@@ -1,17 +1,17 @@
 /**
- * @file saucenao.js
- * @description a command to get your waifu's source
+ * @file Image sourcer command via SauceNAO
  * @author Capuccino
+ * @author Ovyerus
  */
-
 /* eslint-env node */
 
-//this handles the SauceNao handling
-const SauceNAO = require('./sauceQueryHandler');
+const Sagiri = require('@sr229/sagiri');
+
+const urlRegex = /^(?:(?:https?:)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 let sourcer;
 
 exports.init = bot => {
-    sourcer = new SauceNAO(bot.config.sauceKey);
+    sourcer = new Sagiri(bot.config.sauceKey);
 };
 
 exports.commands = [
@@ -19,32 +19,37 @@ exports.commands = [
 ];
 
 exports.saucenao = {
-    desc: 'Gets the image from the recent attachment or via a image link and looks for saucenao to check for the source of the image.',
+    desc: 'Tries to find the source for an image.',
+    usage: '<url or attachment>',
     main(bot, ctx) { 
         return new Promise((resolve, reject) => {
-            if (!ctx.attachments[0] && !ctx.suffix) {
-                ctx.createMessage('saucenao-noImage').then(resolve).catch(reject);
-            } else if (ctx.attachments[0]) {
-                ctx.createMessage('Finding source, this may take some time...').then(() => {
-                    return ctx.channel.sendTyping();
-                }).then(() => {
-                    return sourcer.getSauce(ctx.attachments[0].url);
-                }).then(res => {
-                    return ctx.createMessage('saucenao-found', null, 'channel', {
-                        similarity: res.similarity,
-                        url: res.url
-                    });
-                }).then(resolve).catch(reject);
-            } else if (ctx.suffix) {
-                ctx.createMessage('Finding source, this may take some time...').then(() => {
-                    return ctx.channel.sendTyping();
-                }).then(() => {
-                    return sourcer.getSauce(ctx.suffix);
-                }).then(res => {
-                    return ctx.createMessage('saucenao-found', null, 'channel', {
-                        similarity: res.similarity,
-                        url: res.url
-                    });
+            if (!ctx.attachments[0] && (!ctx.suffix || !urlRegex.test(ctx.suffix))) {
+                ctx.createMessage('Please provide an image.').then(resolve).catch(reject);
+            } else {
+                let url = ctx.attachments[0] ? ctx.attachments[0].url : ctx.suffix;
+
+                ctx.channel.sendTyping().then(() => sourcer.getSource(url)).then(res => {
+                    return ctx.createMessage({embed: {
+                        title: 'Source Found',
+                        description: `[**Source URL**](${res[0].url})`,
+                        thumbnail: {url: res[0].thumbnail},
+                        fields: [
+                            {
+                                name: 'Site',
+                                value: res[0].site,
+                                inline: true
+                            },
+                            {
+                                name: 'Similarity',
+                                value: res[0].similarity.toString(),
+                                inline: true
+                            },
+                            {
+                                name: 'Other Matches',
+                                value: res.slice(1).map(v => `**${v.similarity}** - [${v.site}](${v.url})`).join('\n')
+                            }
+                        ]
+                    }});
                 }).then(resolve).catch(reject);
             }
         });
