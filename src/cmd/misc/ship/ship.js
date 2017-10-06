10d9061af5ccd86834c2547bce3cc7d6fe98e75d
Ovyerus <Ovyerus@users.noreply.github.com>
2017-10-06 20:56:45 +1100

    simplify

--- src/cmd/misc/ship/ship.js
+++ src/cmd/misc/ship/ship.js
@@ -17,8 +17,8 @@ exports.ship = {
             if (ctx.mentions.length > 2 || ctx.args.length > 2) {
                 ctx.createMessage('ship-noArgs').then(resolve).catch(reject);
             } else {
-                let a = ctx.mentions[0] === undefined ? ctx.args[0] : ctx.mentions[0].username;
-                let b = ctx.mentions[1] === undefined ? ctx.args[1] : ctx.mentions[1].username;
+                let a = !ctx.mentions[0] ? ctx.args[0] : ctx.mentions[0].username;
+                let b = !ctx.mentions[1] ? ctx.args[1] : ctx.mentions[1].username;
                 let result = a.substring(0, Math.floor(a.length / 2)) + b.substring(Math.floor(b.length / 2));
 
                 ctx.createMessage('ship', null, 'channel', {result}).then(resolve).catch(reject);
