7fb70b276825d039512be14528a04527b36eeaf1
Ovyerus <Ovyerus@users.noreply.github.com>
2017-10-28 10:15:19 +1100

    Clean up some ambiguities in v1.X command proposal example

--- example/1.x/example.js
+++ example/1.x/example.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 
+// Clara v1.X Command Structure Proposal //
+
 // `Command` and `SubCommand` are defined as children of `global`, so they are accessible without any imports.
 
 class MyCommand {
@@ -7,6 +9,7 @@ class MyCommand {
         this.bot = bot; // Sets the Clara instance onto the command so we don't need to pass it to commands.
         this.description = 'The best command in the world!';
         this.owner = true;
+        this.name = 'overriden' // By default, the command name will be the class name in all lowercase.
         // ...
     }
 
@@ -17,16 +20,23 @@ class MyCommand {
         // ...
     }
 
+    // This is only optional if the command has subcommands, otherwise it is required.
+    async main(ctx) {
+        // ...
+    }
+
     async subcommand() {
         return new SubCommand({
             description: 'The best subcommand in the world!',
             name: 'foo', // This is optional. By default, the name of the method will be used.
-            owner: false // Will override the owner setting of the parent command if its set.
+            owner: false, // Will override the owner setting of the parent command if its set.
+            name: 'banana' // Once again will default to the name of the method lowercased if omitted.
         }, async ctx => { // This is the actual function
             await ctx.createMessage('I am a banana!');
         });
     }
 
+    // Separate commands can be defined with a `static` method, although it is probably better to specify them in a separate class.
     static async separateCommand() {
         return new Command({
             // Same structure as SubCommand.
