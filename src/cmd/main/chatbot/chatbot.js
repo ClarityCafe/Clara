8a79cb16e2214c49d3bedff89cd690532f7f9e8a
Ovyerus <Ovyerus@users.noreply.github.com>
2017-11-01 20:38:20 +1100

    Fix chatbot

--- src/cmd/main/chatbot/chatbot.js
+++ src/cmd/main/chatbot/chatbot.js
@@ -1,26 +1,31 @@
 /**
- * @file Chatbot made using markov chains.
+ * @file Chatbot using AIML.
+ * @author Capuccino
  * @author Ovyerus
  */
+
 const aiml = require('aiml');
+let natsuki;
 
-//Promsifier
-Promise.promisifyAll(aiml.parseDir);
+const parseDir = Promise.promisify(aiml.parseDir);
+Promise.promisifyAll(aiml.AiEngine);
 
-exports.commands = [
-    'chat'
-];
+exports.commands = ['chat'];
+
+exports.init = () => {
+    parseDir(`${mainDir}/assets/chatbot`).then(topics => {
+        natsuki = new aiml.AiEngine('Natsuki', topics, {name: 'Protag-chan'});
+    });
+}
 
 exports.chat = {
     desc: 'Talk to the bot',
     usage: '<message>',
-    main(bot, ctx) {
-        aiml.parseDir(`${mainDir}/assets/chatbot/`).then((err, topics) => {
-            //im sorry
-            const natsuki = new aiml.AiEngine('Natsuki', topics, {name: 'Protag-chan'});
-            natsuki.reply({name: ctx.author}, ctx.suffix, res => {
-                await ctx.createMessage(res);
-            })
-        })
+    async main(bot, ctx) {
+        await ctx.channel.sendTyping();
+
+        let res = await natsuki.reply({name: ctx.author}, ctx.cleanSuffix);
+
+        await ctx.createMessage(res);
     }
-}
+};
