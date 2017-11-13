90c8e0b7dd867f92bba63f866ad9eec25af3d8b2
Capuccino <chinodesuuu@gmail.com>
2017-11-13 14:26:24 +0000

    TRY NOT TO HEADBANG

--- src/cmd/misc/choose/choosemedesu.js
+++ src/cmd/misc/choose/choosemedesu.js
@@ -10,7 +10,7 @@ exports.commands = [
 
 exports.choose = {
     desc: 'Randomly chooses between 2 or more arguments.',
-    usage: '<choice 1>/<choice 2>/[choice .../choice N]',
+    usage: '<choice 1> or <choice 2> or [choice .../choice N]',
     example: 'coke zero/coke',
     async main(bot, ctx) {
         let choices = ctx.suffix.split(' or ');
