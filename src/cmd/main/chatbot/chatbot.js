537405e7413e035d3262c9709a0ac90b54d5c21a
Ovyerus <iamovyerus@gmail.com>
2017-12-06 13:09:17 +1100

    Revert back to markov chain based chatbot

--- src/cmd/main/chatbot/chatbot.js
+++ src/cmd/main/chatbot/chatbot.js
@@ -1,30 +1,55 @@
 /**
- * @file Chatbot using AIML.
- * @author Capuccino
+ * @file Markov chain chatbot.
+ * 
  * @author Ovyerus
  */
 
-const burly = require('burlyy');
-const natsuki = new burly({
-    name: 'Natsuki'
-});
+const Markov = require('markov-strings');
+const fs = require('fs');
 
 exports.commands = ['chat'];
 
-exports.init = () => {
-    natsuki.loadDir(`${mainDir}/assets/chatbot`);
-};
+try {
+    require.resolve(`${__dirname}/lines.txt`);
+} catch(err) {
+    logger.info('Markov file not found. Generating pre-seeded one.');
+    fs.writeFileSync(`${__dirname}/lines.txt`, fs.readFileSync(`${__dirname}/sample.txt`.toString()));
+}
 
 exports.chat = {
-    desc: 'Talk to the bot',
+    desc: 'Talk to the bot. Can be really dumb.',
     usage: '<message>',
     async main(bot, ctx) {
         if (!ctx.suffix) return await ctx.createMessage('chatbot-noArgs');
+        if (/^how (dumb|smart) are you/i.test(ctx.suffix)) {
+            let lines = await readLines();
+            return await ctx.createMessage('chatbot-knowledge', null, 'channel', {
+                amt: lines.length
+            });
+        }
 
         await ctx.channel.sendTyping();
 
-        let res = await natsuki.talk(ctx.cleanSuffix);
+        let lines = await readLines();
+        let chat = new Markov(lines, {stateSize: 3});
 
-        await ctx.createMesage(res);
+        await chat.buildCorpus();
+        await appendLine(ctx.cleanSuffix);
+
+        let res = await chat.generateSentence();
+
+        await ctx.createMessage(res.string);
     }
 };
+
+function readLines() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(`${__dirname}/lines.txt`, (err, data) => err ? reject(err) : resolve(data.toString().split('\n')));
+    });
+}
+
+function appendLine(msg) {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(`${__dirname}/lines.txt`, msg.replace('\n+', ' ').trim() + '\n', err => err ? reject(err) : resolve());
+    });
+}
