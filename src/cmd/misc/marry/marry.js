4af99475403bb013b4351dec51b5c8de093f9dbe
Ovyerus <iamovyerus@gmail.com>
2017-12-13 20:49:24 +1100

    Make marry persistent

--- src/cmd/misc/marry/marry.js
+++ src/cmd/misc/marry/marry.js
@@ -2,8 +2,6 @@
  * @file Marry your imaginary waifu or someone
  * @author Capuccino
  * @author Ovyerus
- * 
- * @todo Persistent marriages
  */
 
 exports.commands = [
@@ -15,20 +13,24 @@ exports.marry = {
     desc: 'Marry someone.',
     usage: '<mention>',
     async main(bot, ctx) {
-        if (ctx.args.length === 0 || ctx.mentionStrings.length === 0) return await ctx.createMessage('marry-noPartner');
-        if (ctx.mentionStrings[0] === ctx.author.id) return await ctx.createMessage('marry-noSelfcest');
-        if (ctx.mentionStrings.length > 1) return await ctx.createMessage('marry-tooManyMentions');
-        if (ctx.mentionStrings[0] === bot.user.id) return await ctx.createMessage('marry-botMentioned');
+        if (ctx.settings.user.partner) return await ctx.createMessage('marry-alreadyWed', null, 'channel', {partner: utils.formatUsername(bot.users.get(ctx.settings.user.partner))});
+        if (!ctx.suffix) return await ctx.createMessage('marry-noPartner');
 
-        await ctx.createMessage('marry-partnerPrompt', null, 'channel', {
+        let user = await bot.lookups.memberLookup(ctx, ctx.suffix);
+
+        if (!user) return;
+        if (user.id === ctx.author.id) return await ctx.createMessage('marry-noSelfcest');
+        if (user.id === bot.user.id) return await ctx.createMessage('marry-selfMentioned');
+
+        await ctx.createMessage('marry-prompt', null, 'channel', {
             author: ctx.author.mention,
-            mentioned: `<@${ctx.mentionStrings[0]}>`
+            partner: user.mention
         });
 
         let msg;
 
         try {
-            msg = await bot.awaitMessage(ctx.channel.id, ctx.mentionStrings[0], () => true, 30000);
+            msg = await bot.awaitMessage(ctx.channel.id, user.id, () => true, 30000);
         } catch(err) {
             return await ctx.createMessage('marry-timeout', null, 'channel', {
                 author: ctx.author.mention
@@ -36,19 +38,24 @@ exports.marry = {
         }
             
         if (/^y(es)?$/i.test(msg.content)) {
-            await ctx.createMessage('marry-marriageSuccess', null, 'channel', {
+            await bot.db[ctx.author.id].partner.set(user.id);
+            await bot.db[user.id].partner.set(ctx.author.id);
+
+            return await ctx.createMessage('marry-success', null, 'channel', {
                 author: ctx.author.mention,
-                mentioned: `<@${ctx.mentionStrings[0]}>`
-            });
-        } else if (/^no?$/i.test(msg.content)) {
-            await ctx.createMessage('marry-marriageFail', null, 'channel', {
-                author: ctx.author.mention
-            });
-        } else {
-            await ctx.createMessage('marry-invalidAnswer', null, 'channel', {
-                mentioned: `<@${ctx.mentionStrings[0]}>`
+                partner: user.mention
             });
         }
+        
+        if (/^no?$/i.test(msg.content)) {
+            return await ctx.createMessage('marry-decline', null, 'channel', {
+                author: ctx.author.mention
+            });
+        }
+
+        await ctx.createMessage('marry-invalidAnswer', null, 'channel', {
+            partner: user.mention
+        });
     }
 };
 
@@ -56,18 +63,29 @@ exports.divorce = {
     desc: 'Divorce your partner.',
     usage: '<mention>',
     async main(bot, ctx) {
-        if (ctx.args.length === 0 || ctx.mentionStrings.length === 0) return await ctx.createMessage('marry-noPartner');
-        if (ctx.mentionStrings[0] === ctx.author.id) return await ctx.createMessage('divorce-noSelfcest');
+        if (!ctx.settings.user.partner) return await ctx.createMessage('divorce-noPartner');
 
-        await ctx.createMessage('divorce-partnerPrompt', null, 'channel', {
+        let user = bot.users.get(ctx.settings.user.partner);
+
+        if (!user) {
+            await bot.db[ctx.author.id].partner.set(null);
+            await bot.db[ctx.settings.user.partner].partner.set(null);
+
+            return await ctx.createMessage('divorce-success', null, 'channel', {
+                author: ctx.author.mention,
+                partner: `<@${ctx.settings.user.partner}>`
+            });
+        }
+
+        await ctx.createMessage('divorce-prompt', null, 'channel', {
             author: ctx.author.mention,
-            mentioned: `<@${ctx.mentionStrings[0]}>`
+            partner: user.mention
         });
 
         let msg;
 
         try {
-            msg = await bot.awaitMessage(ctx.channel.id, ctx.mentionStrings[0], () => true, 30000);
+            msg = await bot.awaitMessage(ctx.channel.id, user.id, () => true, 30000);
         } catch(err) {
             return await ctx.createMessage('marry-timeout', null, 'channel', {
                 author: ctx.author.mention
@@ -75,18 +93,24 @@ exports.divorce = {
         }
 
         if (/^y(es)?$/i.test(msg.content)) {
-            await ctx.createMessage('divorce-divorceSuccess', null, 'channel', {
+            await bot.db[ctx.author.id].partner.set(null);
+            await bot.db[user.id].partner.set(null);
+
+            return await ctx.createMessage('divorce-success', null, 'channel', {
                 author: ctx.author.mention,
-                mentioned: `<@${ctx.mentionStrings[0]}>`
-            });
-        } else if (/^no?$/i.test(msg.content)) {
-            await ctx.createMessage('divorce-divorceFail', null, 'channel', {
-                author: ctx.author.mention
-            });
-        } else {
-            await ctx.createMessage('marry-invalidAnswer', null, 'channel', {
-                mentioned: `<@${ctx.mentionStrings[0]}>`
+                partner: user.mention
             });
         }
+        
+        if (/^no?$/i.test(msg.content)) {
+            return await ctx.createMessage('divorce-fail', null, 'channel', {
+                author: ctx.author.mention
+            });
+        }
+
+        await ctx.createMessage('marry-invalidAnswer', null, 'channel', {
+            partner: user.mention
+        });
+
     }
 };
