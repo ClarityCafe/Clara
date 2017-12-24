2280677a7f72468363622587e15d549c7fe8b452
Capuccino <chinodesuuu@gmail.com>
2017-12-24 06:53:18 +0000

    triple gya

--- src/bot.js
+++ src/bot.js
@@ -12,24 +12,25 @@ const fs = require('fs');
 let config; // = require(`${__dirname}/config.json`);
 
 try {
-    config = require (`${__dirname}/config.json`);
-}catch(_) {
-      config = {
+    config = require(`${__dirname}/config.json`);
+} catch(_) {
+    config = {
       /** @see {Link} https://github.com/ClarityMoe/Clara/issues/133 */
        
         token: process.env.DISCORD_TOKEN,
         debug: process.env.DEBUG,
-        promiseWarnings: process.env.ENABLE_PROMISE_WARNS,
+        promiseWarnings: process.env.ENABLE_PROMISE_WARNS || false,
         ibKey: process.env.IB_TOKEN,
         mainPrefix: process.env.DEFAULT_PREFIX,
         osuApiKey: process.env.OSU_API_TOKEN,
         sauceKey: process.env.SAUCENAO_TOKEN,
+        soundCloudKey: process.env.SOUNDCLOUD_TOKEN
         gameName: process.env.GAME_NAME,
         gameURL: process.env.GAME_URL,
         ownerID: process.env.BOT_OWNER_ID,
         maxShards: process.env.INSTANCES,
-        ytSearchKey: process.env.YOUTUBE_KEY,
-        twitchKey: process.env.TWITCH_KEY,
+        ytSearchKey: process.env.YOUTUBE_TOKEN,
+        twitchKey: process.env.TWITCH_TOKEN,
         nasaKey: process.env.NASA_KEY,
         redisURL: process.env.REDIS_URL || 'redis://127.0.0.1/0'
     };
