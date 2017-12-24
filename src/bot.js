7194d6861b62e8b86e2f1558cf67c56414207ba8
Capuccino <chinodesuuu@gmail.com>
2017-12-24 06:32:29 +0000

    configless Clara (untested)

--- src/bot.js
+++ src/bot.js
@@ -9,7 +9,32 @@
 // Imports
 const Clara = require(`${__dirname}/lib/Clara`);
 const fs = require('fs');
-const config = require(`${__dirname}/config.json`);
+const process = require('process');
+let config; // = require(`${__dirname}/config.json`);
+
+try {
+    config = require (`${__dirname}/config.json`);
+} catch(_) {
+    config = {
+      /** @see {Link} https://github.com/ClarityMoe/Clara/issues/133 */
+       
+      token: process.env.DISCORD_TOKEN,
+      debug: process.env.DEBUG,
+      promiseWarnings: process.env.ENABLE_PROMISE_WARNS,
+      ibKey: process.env.IB_TOKEN,
+      mainPrefix: process.env.DEFAULT_PREFIX,
+      osuApiKey: process.env.OSU_API_TOKEN,
+      sauceKey: process.env.SAUCENAO_TOKEN,
+      gameName: process.env.GAME_NAME,
+      gameURL: process.env.GAME_URL,
+      ownerID: process.env.BOT_OWNER_ID,
+      maxShards: process.env.INSTANCES,
+      ytSearchKey: process.env.YOUTUBE_KEY,
+      twitchKey: process.env.TWITCH_KEY,
+      nasaKey: process.env.NASA_KEY,
+      redisURL: process.env.REDIS_URL || 'redis://127.0.0.1/0'
+    };
+}
 
 // Globals
 global.mainDir = __dirname;
