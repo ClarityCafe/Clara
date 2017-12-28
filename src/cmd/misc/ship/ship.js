4d24684ddd897c9a079d2119b7d3db5c82584fc8
Capuccino <chinodesuuu@gmail.com>
2017-12-28 13:46:17 +0000

    revert

--- src/cmd/misc/ship/ship.js
+++ src/cmd/misc/ship/ship.js
@@ -13,7 +13,7 @@ exports.ship = {
     usage: '<2 names or mentions>',
     async main(bot, ctx) {
         if (ctx.args.length !== 2) return await ctx.createMessage('ship-noArgs');
-        if (ctx.args.length > 2) return await ctx.createMessage('ship-tooManyArgs');
+        else if (ctx.args.length > 2) return await ctx.createMessage('ship-tooManyArgs');
         if (ctx.args[0].toLowerCase() === ctx.args[1].toLowerCase()) return await ctx.createMessage('ship-sameThing');
 
         if (ctx.mentions[0] && new RegExp(`<@!?${ctx.mentions[0].id}>`).test(ctx.args[1])) {
