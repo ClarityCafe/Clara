33070d8c4dbd7617865fb10fd88a174d5b05f426
Capuccino <chinodesuuu@gmail.com>
2017-12-28 13:43:45 +0000

    codeflow

--- src/cmd/misc/ship/ship.js
+++ src/cmd/misc/ship/ship.js
@@ -13,8 +13,8 @@ exports.ship = {
     usage: '<2 names or mentions>',
     async main(bot, ctx) {
         if (ctx.args.length !== 2) return await ctx.createMessage('ship-noArgs');
+        if (ctx.args.length > 2) return await ctx.createMessage('ship-tooManyArgs');
         if (ctx.args[0].toLowerCase() === ctx.args[1].toLowerCase()) return await ctx.createMessage('ship-sameThing');
-        if (ctx.args > 2) return await ctx.createMessage('ship-tooManyArgs');
 
         if (ctx.mentions[0] && new RegExp(`<@!?${ctx.mentions[0].id}>`).test(ctx.args[1])) {
             ctx.mentions[1] = ctx.mentions[0];
