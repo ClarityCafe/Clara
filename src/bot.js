d4220c4f93d573fbcd71670b807a7316caf88305
Capuccino <chinodesuuu@gmail.com>
2018-01-22 03:02:20 +0000

    handle nulls here

--- src/bot.js
+++ src/bot.js
@@ -18,22 +18,22 @@ try {
         /** @see {Link} https://github.com/ClarityMoe/Clara/issues/133 */
        
         token: process.env.DISCORD_TOKEN,
-        debug: process.env.DEBUG,
+        debug: process.env.DEBUG || false,
         promiseWarnings: process.env.ENABLE_PROMISE_WARNS || false,
-        ibKey: process.env.IB_TOKEN,
+        ibKey: process.env.IB_TOKEN || null,
         mainPrefix: process.env.DEFAULT_PREFIX,
-        osuApiKey: process.env.OSU_API_TOKEN,
-        sauceKey: process.env.SAUCENAO_TOKEN,
-        soundCloudKey: process.env.SOUNDCLOUD_TOKEN,
+        osuApiKey: process.env.OSU_API_TOKEN || null,
+        sauceKey: process.env.SAUCENAO_TOKEN || null,
+        soundCloudKey: process.env.SOUNDCLOUD_TOKEN || null,
         gameName: process.env.GAME_NAME || null,
         gameURL: process.env.GAME_URL || null,
         ownerID: process.env.BOT_OWNER_ID,
         maxShards: process.env.INSTANCES || 1,
-        ytSearchKey: process.env.YOUTUBE_TOKEN,
-        discordBotsPWKey: process.env.DISCORD_PW_TOKEN,
-        discordBotsOrgKey: process.env.DISCORD_ORG_TOKEN,
-        twitchKey: process.env.TWITCH_TOKEN,
-        nasaKey: process.env.NASA_KEY,
+        ytSearchKey: process.env.YOUTUBE_TOKEN || null,
+        discordBotsPWKey: process.env.DISCORD_PW_TOKEN || null,
+        discordBotsOrgKey: process.env.DISCORD_ORG_TOKEN || null,
+        twitchKey: process.env.TWITCH_TOKEN || null,
+        nasaKey: process.env.NASA_KEY || null,
         redisURL: process.env.REDIS_URL || 'redis://127.0.0.1/0'
     };
 }
