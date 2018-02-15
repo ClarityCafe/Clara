30cc0ac8bd9b4ac786f05d5c61bf70ce9333b331
Ovyerus <iamovyerus@gmail.com>
2018-02-15 23:05:55 +1100

    Pls fix music issues ;-;

--- src/cmd/main/music/handlers/YouTubeHandler.js
+++ src/cmd/main/music/handlers/YouTubeHandler.js
@@ -3,7 +3,7 @@
  * @author Ovyerus
  */
 
-const ytdl = require('ytdl-core');
+const ytdl = require('youtube-dl');
 const got = require('got');
 
 // List of all itag qualities can be found here: https://en.wikipedia.org/w/index.php?title=YouTube&oldid=800910021#Quality_and_formats.
@@ -16,7 +16,7 @@ class YouTubeHandler {
     async getInfo(url) {
         if (typeof url !== 'string') throw new TypeError('url is not a string.');
 
-        let info = await ytdl.getInfo(url);
+        let info = await new Promise((res, rej) => ytdl.getInfo(url, (err, i) => err ? rej(err) : res(i));
         let res = {
             url,
             title: info.title,
@@ -32,7 +32,7 @@ class YouTubeHandler {
     async getStream(url) {
         if (typeof url !== 'string') throw new TypeError('url is not a string.');
             
-        let info = await ytdl.getInfo(url);
+        let info = await new Promise((res, rej) => ytdl.getInfo(url, (err, i) => err ? rej(err) : res(i));
         let format = info.formats.find(f => f.itag === ITAG) || info.formats.find(f => f.itag === ITAG_FALLBACK);
         format = format ? format.url : info.url; // Fallback to default URL if the wanted itags could not be found;
 
