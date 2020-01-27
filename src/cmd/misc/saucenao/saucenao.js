5222616875b89805152a426877325980d48d8ab4
Ovyerus <iamovyerus@gmail.com>
2020-01-27 16:25:58 +1100

    [saucenao] update to latest Sagiri

--- src/cmd/misc/saucenao/saucenao.js
+++ src/cmd/misc/saucenao/saucenao.js
@@ -4,13 +4,13 @@
  * @author Ovyerus
  */
 
-const Sagiri = require('sagiri');
+const sagiri = require('sagiri');
 
 const urlRegex = /^(?:(?:https?:)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-let sourcer;
+let getSource;
 
 exports.init = bot => {
-    sourcer = new Sagiri(bot.config.tokens.saucenao);
+    getSource = sagiri(bot.config.tokens.saucenao);
 };
 
 exports.commands = [
@@ -35,7 +35,7 @@ exports.saucenao = {
 
         await ctx.channel.sendTyping();
 
-        let res = await sourcer.getSource(url);
+        let res = await getSource(url);
 
         await ctx.createMessage({embed: {
             title: 'Source Found',
